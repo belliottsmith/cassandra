dba371c2e237406b34fb0a712dd4cfafffc841fa
Michael Kjellman <kjellman@apple.com>
2016-01-29 14:48:56 -0800

    rdar://60087619 p24418276 ENV/INF Replace logback with log4j2
    Replace logback with log4j2. Collected together all CIE 3.0 commits
    and squashed.
    
    Additionally upgraded to log4j2 2.13.0.
    
    Cherry-Pick CIE 3.0:
    
    7a35d8fad1e0fc5830d2fb6ce60df48620ce5ba6 removed the logback libraries from lib
    9a31940ed25f0c8c9fcc4048a7493c74e476d2ad removed StatusLoggerTest
        rather than rewrite a log4j version.  Had to make some temporary
        fixes to in-jvm dtest stuff to get everything to compile for now.
        <rdar://problem/35139308> logback -> log4j2 references missed in
        merge to cie-cassandra-3.0.15
    3a15250e46db4e13484913ee68acef4c93311a11 updates for new IDEA workspace settings.
    973c44259b958951d0001601f5a5e68e46282961 (just logging related changes)
      Updated burn tests to use log4j
      Removed logback related things from DatabaseDescriptorReftest
      add org.slf4j.ext to whitelist for scriptbased UDFs - needed for
      access check.
    50dc47432f6f30d7b48c612bfd9a42cb63ed7896 dtests expect C* to output a
      debug.log file but Apple's log4j config doesn't create it
    
    Reference radars
    
    <rdar://problem/27418084> Cass: Port <rdar://problem/24418276> Cass: Replace logback with log4j2 to 3.0.6
    <rdar://problem/51862236> In-JVM dtests free instance classloaders CIE
    <rdar://problem/28826521> Cass: Fix broken log4j2 impl in 3.0
    <rdar://problem/24418496> Cass: Log4j appears to be running in a Servlet environment, but there's no log4j-web module available
    
    (rebased-with-history from commit 3ef8da1626395426d9cf32ed293ad9d005f70abb)
    
    (rebased-with-history from commit fb5f121956c6780d86896227e75c74189378edb9)
    
    (rebased-with-history from commit d77296f317aa87129f0f46a4aff4aaf6261f76ce)
    
    (rebased-with-history from commit ab60f7ebd481d4e428fd85b6830477152d790806)
    
    Squashed rdar://62078675 p24418276 ENV/INF Replace logback with log4j2 - fixup config filename
    
    Correct the system property for finding log4j configuration,
    it should be log4j.configurationFile (not log4j2...)
    
    (rebased-with-history from commit 18a5f32f4d39a3f10b53478f7e642e5bea66bdd7)
    
    (rebased-with-history from commit 5b8f06deb8aa9fa6549c5547b7242389af5ef4dd)
    
    (rebased-with-history from commit 71450e34b802b6de683a823f34bb222dca569ee5)
    
    (rebased-with-history from commit f3c67cd2aaacad2cdef8ae0222a52885d060f6d6)

--- test/distributed/org/apache/cassandra/distributed/impl/InstanceIDDefiner.java
+++ test/distributed/org/apache/cassandra/distributed/impl/InstanceIDDefiner.java
@@ -18,13 +18,16 @@
 
 package org.apache.cassandra.distributed.impl;
 
-import ch.qos.logback.core.PropertyDefinerBase;
 import org.apache.cassandra.concurrent.NamedThreadFactory;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.lookup.StrLookup;
 
 /**
- * Used by logback to find/define property value, see logback-dtest.xml
+ * Used by log4j to find/define property value, see log4j2-dtest.xml
  */
-public class InstanceIDDefiner extends PropertyDefinerBase
+@Plugin(name="instance", category = StrLookup.CATEGORY)
+public class InstanceIDDefiner implements StrLookup
 {
     // Instantiated per classloader, set by Instance
     private static volatile String instanceId = "<main>";
@@ -34,7 +37,17 @@ public class InstanceIDDefiner extends PropertyDefinerBase
         NamedThreadFactory.setGlobalPrefix("node" + id + "_");
     }
 
-    public String getPropertyValue()
+    public String lookup(String s)
+    {
+        return lookup();
+    }
+
+    public String lookup(LogEvent logEvent, String s)
+    {
+        return lookup();
+    }
+
+    private String lookup()
     {
         return instanceId;
     }
