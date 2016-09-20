f57f170fba0cea3920ecfabf72ab0de0bea2644c
Richard Low <ral@apple.com>
2016-09-20 16:47:58 -0700

    rdar://60088234 p25376022 OPS/PRF Aggressive leveled compaction
    Add Aggresive GC to LCS enabled by system property
    cassandra.aggressivegcls.enabled
    
    CIE 3.0 Commits / Radars
    
    bfd7987084798bf7c16ce2a25731f7885e6345d8 <rdar://problem/25376022>
    Cass: can we clean up deleted data on content quicker?
    
    Updated for changes to metadata/repair metadata.
    
    dc459bc2af4ee32849a6764309b5133eabac2c98 <rdar://problem/30557344>
    Cass: address Marcus aggressive compaction concerns
    
    Squashed CIE4 review commits
     508fb73ba9 fixup: review nit
     e4ddba184a fixup: nit from Jordan

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -156,6 +156,8 @@ public class DatabaseDescriptor
                                        ? new CommitLogSegmentManagerCDC(c, DatabaseDescriptor.getCommitLogLocation())
                                        : new CommitLogSegmentManagerStandard(c, DatabaseDescriptor.getCommitLogLocation());
 
+    private static volatile boolean aggressiveGC = Boolean.getBoolean("cassandra.aggressivegcls.enabled");
+
     public static void daemonInitialization() throws ConfigurationException
     {
         daemonInitialization(DatabaseDescriptor::loadConfig);
@@ -3093,6 +3095,10 @@ public class DatabaseDescriptor
         int seconds = conf.validation_preview_purge_head_start_in_sec;
         return Math.max(seconds, 0);
     }
+
+    public static void setEnableAggressiveGCCompaction(boolean enable) { aggressiveGC = enable; }
+
+    public static boolean getEnableAggressiveGCCompaction() { return aggressiveGC; }
 
     public static boolean disableIncrementalRepair()
     {
