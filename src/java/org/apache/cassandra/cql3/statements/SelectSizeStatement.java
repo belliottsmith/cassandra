54394c96b14dd6a020aa1db3ba28a7e4f446c75e
Blake Eggleston <bdeggleston@gmail.com>
2016-09-28 14:20:44 -0700

    rdar://60088311 p26203712 PRF/FTR SELECT_SIZE statement
    Add SELECT_SIZE statement to return the size of an individual partition.
    
    CIE 3.0 Commit / Radars
    
    1f6635186cdd0a34a8fb2a6e06f41e30472528cd <rdar://problem/2620371>
    Updated for changes to ReplicaPlans, internode messaging and replaced the
    PartitionSizeCallback test with an in-JVM dtest.
    
    c045444ff0d0d92bd13be09f95c9151e910ef81b  <rdar://problem/30713714>
    ops: clouddb: instances are restarting due to jvm crashes
    
    256f3d0417f11b11e4c3057f12f298f291a80f00
    Dont use View.select(CANONICAL..) as that is apparently only for
    live sstables
    
    0544f1c45c9d25f6572d34f296a734deee562ba8  <rdar://problem/31118255>
    Add storage proxy metrics for PartitionSizeCommand (#732) updated for
    the new replica planning
    
    dd9c7d0d88769b9005bd61dec411856077845ff4  <rdar://problem/32937278>
    fix partition size command performance, use cfs.readOrdering like read
    path as opposed to referencing sstables  (#864) PARTITION_SIZE
    
    * Use cfs.readOrdering like read path as opposed to referencing sstables
    <rdar://problem/32937278> Problem with PartitionSize commands in 2.1.17
    
    CIE4 Initial review commits
    81759a4ca6 fixup: review comments
    6e33209280 fixup: review feedback from David/Blake
    e7c8383f87 fixup: removed special partition size request timeout and moved callback with other reads
    266b3bc41e fixup: review feedback two from blake
    
    (rebased-with-history from commit 97a7281792a2f37fde25bcb5c6e790e503298e2c)
    
    (rebased-with-history from commit 2b36f44f580db249b9da5793d56ae996fdbdde66)
    
    (rebased-with-history from commit 4bf961e9dd9785330522e34b09b868e560a81a23)
    
    (rebased-with-history from commit 2035a662e2ed35aa29662646e8c634f9486dc1b5)
    
    (rebased-with-history from commit 18b59acca4e4fd1db0f13355e980484787911ab3)
    
    (rebased-with-history from commit bd0f6a2bdced8eac83b38d9ede80067c8c9339b1)
    
    (rebased-with-history from commit 730a7f911dd34c13abf3360aba47c86c752bd608)
    
    (rebased-with-history from commit d54d192f65c0b45275d49588374ed1c6fd5f795d)
    
    (rebased-with-history from commit 34402237921bf51974b518b7cf1fe514059d5bb8)
    
    (rebased-with-history from commit 175c83d0ae8d4fbd23bc5fffe2a5d7e729874be8)
    
    (rebased-with-history from commit cce8fd006be2f0891f30d3ed91f8511912f8ddeb)
    
    (rebased-with-history from commit d69c970b00ca963190b6a388f2bc87335890616a)
    
    rdar://74207044 (Minimize number of new reserved keywords introduced in 4.0)
    
    Minimize the number of new reserved keywords introduced since CIE 3.0.19.
    This is upstreamed as CASSANDRA-16439 and most of this patch will drop
    out next time this is rebased against OSS.
    
    The ACI Cassandra specific SELECT_SIZE unreserved word will remain, and should be
    merged into the SELECT_SIZE patch itself.
    
    (rebased-with-history from commit f3a71940f896593136be1e53f1f1c9237cb23d28)
    
    (rebased-with-history from commit a4ac201245ed8a8889d6ad493bb41d714a548782)
    
    (rebased-with-history from commit 149fb0bf9f2afc3259e4748326ee8294e6c9b852)
    
    rdar://62542912 (Update SELECT_SIZE message ids once Xmas patch is merged)
    
    Update SELECT_SIZE to use new CUSTOM ids when running under 4.0 and
    downgrade to the original ids when communication with 3.0 nodes.
    
    Add callback on failure handling.
    
    Refactor the jvm dtest to an upgrade test and add checks for failure
    using ByteBuddy to provoke the failure.
    
    (rebased-with-history from commit d3b7ecd3628cc6981b621ddbe20c9e0548a86de2)
    
    (rebased-with-history from commit bdb038a3846e57fb68a75ddd4f2e8e52e9dd622a)
    
    fixup: rdar://62542912 (Update SELECT_SIZE message ids once Xmas patch is merged)
    
    Disable autocompaction and on upgraded nodes, force compaction.  Since
    picking up CASSANDRA-16619 the commit log replayer may build a redundant
    sstable from pre-nb format sstables do not include a host-id.  The extra
    sstables increase the size of the entries and cause the test to fail.

---
+++ src/java/org/apache/cassandra/cql3/statements/SelectSizeStatement.java
@@ -0,0 +1,230 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.cql3.statements;
+
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import org.apache.cassandra.audit.AuditLogContext;
+import org.apache.cassandra.audit.AuditLogEntryType;
+import org.apache.cassandra.auth.Permission;
+import org.apache.cassandra.cql3.CQLStatement;
+import org.apache.cassandra.cql3.ColumnIdentifier;
+import org.apache.cassandra.cql3.ColumnSpecification;
+import org.apache.cassandra.cql3.QualifiedName;
+import org.apache.cassandra.cql3.QueryOptions;
+import org.apache.cassandra.cql3.ResultSet;
+import org.apache.cassandra.cql3.VariableSpecifications;
+import org.apache.cassandra.cql3.WhereClause;
+import org.apache.cassandra.cql3.functions.Function;
+import org.apache.cassandra.cql3.restrictions.StatementRestrictions;
+import org.apache.cassandra.db.ConsistencyLevel;
+import org.apache.cassandra.db.PartitionSizeCommand;
+import org.apache.cassandra.db.marshal.InetAddressType;
+import org.apache.cassandra.db.marshal.LongType;
+import org.apache.cassandra.db.view.View;
+import org.apache.cassandra.exceptions.InvalidRequestException;
+import org.apache.cassandra.exceptions.RequestExecutionException;
+import org.apache.cassandra.exceptions.RequestValidationException;
+import org.apache.cassandra.exceptions.UnauthorizedException;
+import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.schema.Schema;
+import org.apache.cassandra.schema.TableMetadata;
+import org.apache.cassandra.schema.TableMetadataRef;
+import org.apache.cassandra.serializers.InetAddressSerializer;
+import org.apache.cassandra.serializers.LongSerializer;
+import org.apache.cassandra.service.ClientState;
+import org.apache.cassandra.service.QueryState;
+import org.apache.cassandra.service.StorageProxy;
+import org.apache.cassandra.transport.messages.ResultMessage;
+
+import static org.apache.cassandra.cql3.statements.RequestValidations.checkNotNull;
+import static org.apache.cassandra.cql3.statements.RequestValidations.checkTrue;
+
+/**
+ * Provides client with a method determining the size of a partition on disk for a given row
+ *
+ * syntax is: SELECT_SIZE FROM <keyspace>.<table> WHERE <partition_key>=<some_val>;
+ *
+ * see: <rdar://problem/26203712> Add API to request the size of a row
+ */
+public class SelectSizeStatement implements CQLStatement
+{
+    private static final ColumnIdentifier ENDPOINT_IDENTIFIER = new ColumnIdentifier("endpoint", true);
+    private static final ColumnIdentifier SIZE_IDENTIFIER = new ColumnIdentifier("size (bytes)", true);
+
+    public final VariableSpecifications bindVariables;
+    private final TableMetadata table;
+    private final StatementRestrictions restrictions;
+
+    public SelectSizeStatement(TableMetadata table,
+                               VariableSpecifications bindVariables,
+                               StatementRestrictions restrictions)
+    {
+        this.table = table;
+        this.bindVariables = bindVariables;
+        this.restrictions = restrictions;
+    }
+
+    @Override
+    public List<ColumnSpecification> getBindVariables()
+    {
+        return bindVariables.getBindVariables();
+    }
+
+    public void validate(ClientState state) throws RequestValidationException
+    {
+    }
+
+    public AuditLogContext getAuditLogContext()
+    {
+        return new AuditLogContext(AuditLogEntryType.SELECT_SIZE, keyspace(), table.name);
+    }
+
+    private ByteBuffer getKey(QueryOptions options) throws InvalidRequestException
+    {
+        List<ByteBuffer> keys = restrictions.getPartitionKeys(options);
+        checkTrue(keys.size() == 1, "SELECT SIZE statements can only be restricted to a single partition key.");
+        return keys.get(0);
+    }
+
+    @VisibleForTesting
+    PartitionSizeCommand createCommand(QueryOptions options) throws InvalidRequestException
+    {
+        return new PartitionSizeCommand(table.keyspace, table.name, getKey(options));
+    }
+
+    public ResultMessage execute(QueryState state, QueryOptions options, long queryStartNanoTime)
+    {
+        ConsistencyLevel cl = options.getConsistency();
+        checkNotNull(cl, "Invalid empty consistency level");
+
+        cl.validateForRead();
+
+        return executeInternal(state, options, queryStartNanoTime);
+    }
+
+    public ResultMessage.Rows executeLocally(QueryState state, QueryOptions options) throws RequestExecutionException, RequestValidationException
+    {
+        return executeInternal(state, options, System.nanoTime());
+    }
+
+    private ResultMessage.Rows executeInternal(QueryState state, QueryOptions options, long queryStartNanoTime) throws RequestValidationException, RequestExecutionException
+    {
+        if (options.getConsistency().isSerialConsistency())
+        {
+            throw new InvalidRequestException("SELECT_SIZE statements cannot use SERIAL consistency");
+        }
+        Map<InetAddressAndPort, Long> sizes = StorageProxy.fetchPartitionSize(createCommand(options), options.getConsistency(), queryStartNanoTime);
+        return convertToRowsResultSet(sizes);
+    }
+
+    public String keyspace()
+    {
+        return table.keyspace;
+    }
+
+    public String columnFamily()
+    {
+        return table.name;
+    }
+
+    private ResultMessage.Rows convertToRowsResultSet(Map<InetAddressAndPort, Long> sizesPerReplica)
+    {
+        Set<Map.Entry<InetAddressAndPort, Long>> resultEntries = sizesPerReplica.entrySet();
+        List<List<ByteBuffer>> rows = new ArrayList<>(resultEntries.size());
+        for (Map.Entry<InetAddressAndPort, Long> entry : resultEntries)
+        {
+            rows.add(getSerializedRowEntry(entry));
+        }
+        return new ResultMessage.Rows(new ResultSet(new ResultSet.ResultMetadata(getColumnSpecifications()), rows));
+    }
+
+    private List<ByteBuffer> getSerializedRowEntry(Map.Entry<InetAddressAndPort, Long> entry)
+    {
+        List<ByteBuffer> row = new ArrayList<>(2);
+        row.add(InetAddressSerializer.instance.serialize(entry.getKey().address));
+        row.add(LongSerializer.instance.serialize(entry.getValue()));
+        return row;
+    }
+
+    private List<ColumnSpecification> getColumnSpecifications()
+    {
+        List<ColumnSpecification> specifications = new ArrayList<>(2);
+        specifications.add(new ColumnSpecification(table.keyspace, table.name, ENDPOINT_IDENTIFIER, InetAddressType.instance));
+        specifications.add(new ColumnSpecification(table.keyspace, table.name, SIZE_IDENTIFIER, LongType.instance));
+        return specifications;
+    }
+
+    public void authorize(ClientState state)
+    {
+        if (table.isView())
+        {
+            TableMetadataRef baseTable = View.findBaseTable(keyspace(), columnFamily());
+            if (baseTable != null)
+                state.ensureTablePermission(baseTable, Permission.SELECT);
+        }
+        else
+        {
+            state.ensureTablePermission(table, Permission.SELECT);
+        }
+
+        for (Function function : getFunctions())
+            state.ensurePermission(Permission.EXECUTE, function);
+    }
+
+    public static class RawStatement extends QualifiedStatement
+    {
+        private final WhereClause whereClause;
+
+        public RawStatement(QualifiedName cfName,
+                            WhereClause whereClause)
+        {
+            super(cfName);
+            this.whereClause = whereClause;
+        }
+
+        public SelectSizeStatement prepare(ClientState state) throws RequestValidationException
+        {
+            TableMetadata table = Schema.instance.validateTable(keyspace(), name());
+            checkTrue(!table.isView(), "SELECT SIZE statement can not be used on views");
+
+            StatementRestrictions restrictions = parseAndValidateRestrictions(table, bindVariables);
+            return new SelectSizeStatement(table, bindVariables, restrictions);
+        }
+
+        private StatementRestrictions parseAndValidateRestrictions(TableMetadata table, VariableSpecifications boundNames)
+        {
+            StatementRestrictions restrictions = new StatementRestrictions(StatementType.SELECT, table, whereClause, boundNames, false, false, false, false);
+            // The WHERE clause can only restrict the query to a single partition (nothing more restrictive, nothing less restrictive).
+            checkTrue(restrictions.hasPartitionKeyRestrictions(), "SELECT SIZE statements must be restricted to a partition.");
+            checkTrue(!restrictions.hasClusteringColumnsRestrictions(), "SELECT SIZE statements can only have partition key restrictions.");
+            checkTrue(!restrictions.hasNonPrimaryKeyRestrictions(), "SELECT SIZE statements can only have partition key restrictions.");
+            return restrictions;
+        }
+    }
+}
+
