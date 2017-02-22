1472b730860ff3acecf8225a362d0d048fa34884
Jeff Jirsa <jjirsa@apple.com>
2017-02-22 10:53:11 -0800

    rdar://60088260 p30318758 PRO/RST Add configuration to disable ALTER TABLE
    CIE 3.0 Commits / Radars
    da01e55338da1536b957c46b049bc901655b77ce  <rdar://problem/30318758>
    Disable ALTER TABLE ability to add/remove columns from tables to prevent CASSANDRA-13004 corruption
    Refactored for changes to parser, and case for alter type no longer necessary after CASSANDRA-13426.
    
    9de633a4f5cf550ae315a19afe5d7cced88a70c8  <rdar://problem/37369311>
    don't block adding columns by default (#1025)
    
    CIE4 Review squashed commits
     d78600f145a5bf4faf1b464cc2ab25efa06841f9 add more test cases

--- src/java/org/apache/cassandra/service/StorageServiceMBean.java
+++ src/java/org/apache/cassandra/service/StorageServiceMBean.java
@@ -771,4 +771,10 @@ public interface StorageServiceMBean extends NotificationEmitter
      */
     public void stopFullQueryLogger();
 
+    /** Returns boolean representing whether or not alter table is enabled */
+    public boolean getAlterTableEnabled();
+
+    /** Sets the boolean representing whether or not alter table is enabled */
+    public void setAlterTableEnabled(boolean alterTableEnabled);
+
 }
