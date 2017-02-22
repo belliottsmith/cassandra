759481e125007b73f9a0c8de801cfea22621ea08
Jeff Jirsa <jjirsa@apple.com>
2017-02-22 10:53:11 -0800

    rdar://60088260 p30318758 PRO/RST Add configuration to disable ALTER TABLE
    CIE 3.0 Commits / Radars
    da01e55338da1536b957c46b049bc901655b77ce  <rdar://problem/30318758>
    Disable ALTER TABLE ability to add/remove columns from tables to prevent CASSANDRA-13004 corruption
    Refactored for changes to parser, and case for alter type no longer necessary after CASSANDRA-13426.
    
    9de633a4f5cf550ae315a19afe5d7cced88a70c8  <rdar://problem/37369311>
    don't block adding columns by default (#1025)
    
    CIE4 Review squashed commits
     d78600f145a5bf4faf1b464cc2ab25efa06841f9 add more test cases
    
    (rebased-with-history from commit 1472b730860ff3acecf8225a362d0d048fa34884)
    
    (rebased-with-history from commit 2cf0669748e91c62c1a6ffe75f2cca80329c52fb)
    
    (rebased-with-history from commit 51b74d41ad8069ee310fa516acf5510234be404c)
    
    (rebased-with-history from commit 6339f41ef731e0d8e5d729934d6355f068131086)
    
    (rebased-with-history from commit 93901e6ecc928f2f21d6fd5832bc74c5adefe4bf)
    
    (rebased-with-history from commit c60d68733650e2277293ffff4babb8bd5f16e9c7)
    
    (rebased-with-history from commit 5b1ec21e59d769284c4a4855a103facbd55bac83)
    
    (rebased-with-history from commit 457cb46b7acdf17260fc56a4bb2701fbbb76b045)

--- src/java/org/apache/cassandra/cql3/statements/schema/AlterTableStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/schema/AlterTableStatement.java
@@ -29,17 +29,12 @@ import org.apache.cassandra.audit.AuditLogContext;
 import org.apache.cassandra.audit.AuditLogEntryType;
 import org.apache.cassandra.auth.Permission;
 
-import org.apache.cassandra.cql3.CQL3Type;
-import org.apache.cassandra.cql3.CQLStatement;
-import org.apache.cassandra.cql3.ColumnIdentifier;
-import org.apache.cassandra.cql3.QualifiedName;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.cql3.*;
 import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.marshal.AbstractType;
-
-import org.apache.cassandra.schema.ColumnMetadata;
-import org.apache.cassandra.schema.IndexMetadata;
-import org.apache.cassandra.schema.KeyspaceMetadata;
-import org.apache.cassandra.schema.Keyspaces;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.schema.*;
 import org.apache.cassandra.schema.Keyspaces.KeyspacesDiff;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.schema.TableParams;
@@ -106,6 +101,17 @@ public abstract class AlterTableStatement extends AlterSchemaStatement
 
     abstract KeyspaceMetadata apply(KeyspaceMetadata keyspace, TableMetadata table);
 
+    protected void checkAlterTableEnabled()
+    {
+        // Protect against race conditions in 3.0 since the 8099 rewrite, where
+        // columns being added (and perhaps removed) to(from) tables can cause data corruption.
+        // See CASSANDRA-13004 for details
+        if(!DatabaseDescriptor.getAlterTableEnabled())
+        {
+            throw new ConfigurationException("Error while altering table: modifying column definitions is not allowed in Apple's version of Cassandra");
+        }
+    }
+
     /**
      * ALTER TABLE <table> ALTER <column> TYPE <newtype>;
      *
@@ -154,6 +160,8 @@ public abstract class AlterTableStatement extends AlterSchemaStatement
 
         public KeyspaceMetadata apply(KeyspaceMetadata keyspace, TableMetadata table)
         {
+            checkAlterTableEnabled(); // Check CIE-specific configuration
+
             TableMetadata.Builder tableBuilder = table.unbuild();
             Views.Builder viewsBuilder = keyspace.views.unbuild();
             newColumns.forEach(c -> addColumn(keyspace, table, c, tableBuilder, viewsBuilder));
@@ -242,6 +250,8 @@ public abstract class AlterTableStatement extends AlterSchemaStatement
 
         public KeyspaceMetadata apply(KeyspaceMetadata keyspace, TableMetadata table)
         {
+            checkAlterTableEnabled(); // Check CIE-specific configuration
+
             TableMetadata.Builder builder = table.unbuild();
             removedColumns.forEach(c -> dropColumn(keyspace, table, c, builder));
             return keyspace.withSwapped(keyspace.tables.withSwapped(builder.build()));
@@ -304,6 +314,8 @@ public abstract class AlterTableStatement extends AlterSchemaStatement
 
         public KeyspaceMetadata apply(KeyspaceMetadata keyspace, TableMetadata table)
         {
+            checkAlterTableEnabled(); // Check CIE-specific configuration
+
             TableMetadata.Builder tableBuilder = table.unbuild();
             Views.Builder viewsBuilder = keyspace.views.unbuild();
             renamedColumns.forEach((o, n) -> renameColumn(keyspace, table, o, n, tableBuilder, viewsBuilder));
