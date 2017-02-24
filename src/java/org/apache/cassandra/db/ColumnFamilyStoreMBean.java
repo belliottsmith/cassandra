66c059d459ce72ee3c5f750ab8c432727c6de225
Nachiket Patil <nachiket_patil@apple.com>
2017-02-23 18:27:10 -0800

    rdar://60088169 p30198445 OPS/MTR Add CAS/ConditionNot metrics, track recent latency
    Cherry-Pick CIE 3.0 / Radars
    
    3f238d93f21b2e332fb69edeea7c3cec9b45d0c1 <rdar://problem/30198445>
    Mgr: Add support to aggregate and publish Table metrics in Cassandra 3.0
    Some minor adjustments for CAS refactor.
    
    72af53cea435e74dfddd71ba54c3032b784d9a8f <rdar://problem/32298967>
    Cass: fix client request consistency level metrics in 3.0 (#835)
    
    9063cd8333c7973561dc2f1b9700ff6ebfc44ea4 <rdar://problem/32298967>
    Cass: fix client request consistency level metrics in 3.0
    
    Cherry-Pick: CIE 3.0
    
    (rebased-with-history from commit 2fe640b9bb64ab9239cbf45126a554213cc51d3a)
    
    (rebased-with-history from commit 71311f979a5775c870a647aa10a8c9b07fdaf685)
    
    (rebased-with-history from commit 30ffef4d743e9a76c99e18da0811ff4f537ecc7e)
    
    (rebased-with-history from commit 1fe12a57c76d706a1dc68845435a251baa75b77a)
    
    (rebased-with-history from commit 875300e719abab96b1eebb19143a929e81443353)
    
    (rebased-with-history from commit 0f0432a61e142a041dd08e0f5fbd1e69a87d3ace)
    
    (rebased-with-history from commit 4556b6714c1b431b9cce139de4888e4104ceeb75)
    
    (rebased-with-history from commit a6273d110223077bf8663dfdf4c55ddecadfeaa2)
    
    (rebased-with-history from commit 2f83b939c449a6c63c783fc2d21131ff3cb71ed7)
    
    (rebased-with-history from commit bba3e17e16f9ae10d0cb987c6c4f6b1aefb07ad8)
    
    (rebased-with-history from commit 8aebcd0b83be216264492e0413a32709ef601ed5)
    
    (rebased-with-history from commit 6f339becf114ea8e6d5e4c61dd786868919d2c17)
    
    (rebased-with-history from commit 02342633f895bb6329ec56ea17d3511b7ad28aa3)
    
    (rebased-with-history from commit 60e74d17f611ab952e143de7f150d0e41a23b8c5)
    
    (rebased-with-history from commit 6cf7d80f8787f81f3fa2e56674dcc61d1ae8d2cc)
    
    rdar://76613627 Metrics CASClientRequestMetrics#conditionNotMet is not used (#2344)
    
    (rebased-with-history from commit 780c431405e2032817d55dd987aee4a34b7afbbc)
    
    (rebased-with-history from commit 7cb8fc3c4df00f7355eabdcfb8e7e3ce12c75f16)

--- src/java/org/apache/cassandra/db/ColumnFamilyStoreMBean.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStoreMBean.java
@@ -251,4 +251,57 @@ public interface ColumnFamilyStoreMBean
     public boolean hasMisplacedSSTables();
 
     public List<String> getDataPaths() throws IOException;
+
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#sstablesPerReadHistogram
+     * @return a histogram of the number of sstable data files accessed per read: reading this property resets it
+     */
+    public long[] getRecentSSTablesPerReadHistogramV3();
+
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#sstablesPerReadHistogram
+     * @return a histogram of the number of sstable data files accessed per read
+     */
+    public long[] getSSTablesPerReadHistogramV3();
+
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#readLatency
+     * @return an array representing the latency histogram
+     */
+    public long[] getRecentReadLatencyHistogramMicrosV3();
+
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#writeLatency
+     * @return an array representing the latency histogram
+     */
+    public long[] getRecentWriteLatencyHistogramMicrosV3();
+
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#rangeLatency
+     * @return an array representing the latency histogram
+     */
+    public long[] getRecentRangeLatencyHistogramMicrosV3();
+
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#estimatedPartitionSizeHistogram
+     */
+    public long[] getEstimatedRowSizeHistogram();
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#estimatedColumnCountHistogram
+     */
+    public long[] getEstimatedColumnCountHistogram();
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#casPrepare
+     */
+    public long[] getCasPrepareLatencyHistogram();
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#casPropose
+     */
+    public long[] getCasProposeLatencyHistogram();
+    /**
+     * @see org.apache.cassandra.metrics.TableMetrics#casCommit
+     */
+    public long[] getCasCommitLatencyHistogram();
+
+
 }
