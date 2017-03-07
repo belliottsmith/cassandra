4580b7542f4d2eff3cccd3ae77906d81e9a04ebc
Marcus Eriksson <marcus_eriksson@apple.com>
2017-03-07 10:05:23 +0100

    rdar://60088314 p30735785 PRO/FTR Quota for keyspaces
    Track quota for keyspaces in cie_internal.ks_quota and enforce
    on upsert operations.
    
    CIE 3.0 Commits / Radars
    
    4773dc0ab042d1d707b6b62ab5a4c897818a814d  <rdar://problem/30735785>
    updated for refactors to statements/permissions and schema.
    
    597d799ee0addffec07126777a2b882f1f330025  <rdar://problem/38086987>
    Allow a yaml prop to automatically insert the limit for a given keyspace.
    
    (rebased-with-history from commit 9464018fdfd1ebe5da3df9f2d48b3fb6bcec0d41)
    
    (rebased-with-history from commit 4f1dd2eed1798fd8ced3afb69e2b79e2857b38a1)
    
    (rebased-with-history from commit 5f027afaf9021719bb92f2146bdb20d3ffac9de1)
    
    (rebased-with-history from commit d75657411d848dbc8350b486cc227451e0f16017)
    
    (rebased-with-history from commit 3c7452fb65887078ae8cac28889d117817b5999d)
    
    (rebased-with-history from commit 6e71a25f8f81ad412e38bce69971e99acd20bbf3)
    
    (rebased-with-history from commit a81e80a85c2ab6684c6d96386ce9c6fb844e551f)
    
    (rebased-with-history from commit 99188905fecab5aafbfccc940cea69fa97d9663c)

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -454,6 +454,8 @@ public class CassandraDaemon
 
         initializeClientTransports();
 
+        KeyspaceQuota.scheduleQuotaCheck();
+
         if (DatabaseDescriptor.getAuthCacheWarmingEnabled())
             AuthCacheService.instance.warmCaches();
         else
