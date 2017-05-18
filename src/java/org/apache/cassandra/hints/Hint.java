e91294421f5cc5c14cf119700dc613a18a269629
Marcus Eriksson <marcus_eriksson@apple.com>
2017-05-18 07:53:53 +0200

    rdar://60088187 p32149498 PRO/DFT Set default maxHintTTL to 86400
    <rdar://problem/32149498> Set default maxHintTTL to 86400 in Cassandra
    
    Cherry Pick CIE 3.0 757b587b5e377320338af828b2a1a31a823df8c1
    
    (rebased-with-history from commit 2da9784c9a30ccfa42965d1a6241b1a7e890e3c8)
    
    (rebased-with-history from commit 25a9cd31a56d8907ea1ffc7acafe3e294aa36e77)
    
    (rebased-with-history from commit 050b8d7f7780adfd0d27dd5e87fd62295198804d)
    
    (rebased-with-history from commit b32d5c3f646d4372c3da7e8d42112804c21c1fe6)
    
    (rebased-with-history from commit dbf531652a6d2fe6f9d70a78ebf72a8b2f705db3)
    
    (rebased-with-history from commit 02d9ffd1dde0dee327530f9260ea486eb2a678f5)
    
    (rebased-with-history from commit f0e8b163df300e286a145c20ce1eeaabbbf2ce18)
    
    (rebased-with-history from commit c1a14c41c6fa1aea5d399fdc30bb568be98347fa)

--- src/java/org/apache/cassandra/hints/Hint.java
+++ src/java/org/apache/cassandra/hints/Hint.java
@@ -57,7 +57,7 @@ import static org.apache.cassandra.db.TypeSizes.sizeofUnsignedVInt;
 public final class Hint
 {
     public static final Serializer serializer = new Serializer();
-    static final int maxHintTTL = Integer.getInteger("cassandra.maxHintTTL", Integer.MAX_VALUE);
+    static final int maxHintTTL = Integer.getInteger("cassandra.maxHintTTL", (int)TimeUnit.DAYS.toSeconds(1));
 
     final Mutation mutation;
     final long creationTime;  // time of hint creation (in milliseconds)
