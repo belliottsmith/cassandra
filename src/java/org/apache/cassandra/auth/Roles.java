ca26f3d71055562fc05cbb79ce94ec83cc045c45
Sam Tunnicliffe <stunnicliffe@apple.com>
2017-10-25 23:58:22 +0100

    rdar://60088304 p35097544 OPS/PRF Reverse iteration order of resources during permissions checking
    CIE 3.0 Commit / Radars
    
    70f792462a9e5328c55463ecc5c186a168787bad <rdar://problem/35097544>
    Reverse iteration order of resources during permissions checking (#962)
    
    CIE4 Initial review commits
     88d0c449d9 fixup: remove resolved comment
    
    (rebased-with-history from commit eabe6dae04b0013a6a18eb4b052e661fc84805df)
    
    (rebased-with-history from commit f7a952cefc49ea0e6f3f78141df211ead6c1d414)
    
    (rebased-with-history from commit 51b4b20e285845ed0a50f8454627fc2f0c6ebb5b)
    
    (rebased-with-history from commit 07118a79ae94fcbb3de2d1778d751c228a64bac9)
    
    (rebased-with-history from commit 6064d4d94da30cc3dbbd0a8e4fa86c43d4057c83)
    
    (rebased-with-history from commit 203d86ac03acbd8b32a93d7c8432f9d25494c864)
    
    (rebased-with-history from commit 65d5600c13c691b2bd3f6ec81611bebce2ee21f3)
    
    (rebased-with-history from commit 4330289a59edd82f861c6aab499d68a218d258a1)
    
    (rebased-with-history from commit 0a4dc6f9fc08adbe5e705da8a1d46c013536cffb)
    
    (rebased-with-history from commit 3e5dd520e876ab22354cae57c2e1ae71cadb1931)

--- src/java/org/apache/cassandra/auth/Roles.java
+++ src/java/org/apache/cassandra/auth/Roles.java
@@ -58,6 +58,12 @@ public class Roles
     }
 
     @VisibleForTesting
+    public static void initRolesCache(RolesCache cache)
+    {
+        Roles.cache = cache;
+    }
+
+    @VisibleForTesting
     public static void clearCache()
     {
         cache.invalidate();
