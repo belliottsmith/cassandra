b71078223887d148e179f742a01b0c7782cbc444
Sam Tunnicliffe <stunnicliffe@apple.com>
2017-10-25 23:58:22 +0100

    rdar://60088304 p35097544 OPS/PRF Reverse iteration order of resources during permissions checking
    CIE 3.0 Commit / Radars
    
    70f792462a9e5328c55463ecc5c186a168787bad <rdar://problem/35097544>
    Reverse iteration order of resources during permissions checking (#962)
    
    CIE4 Initial review commits
     88d0c449d9 fixup: remove resolved comment
    
    (rebased-with-history from commit eabe6dae04b0013a6a18eb4b052e661fc84805df)
    
    (rebased-with-history from commit f7a952cefc49ea0e6f3f78141df211ead6c1d414)
    
    (rebased-with-history from commit 51b4b20e285845ed0a50f8454627fc2f0c6ebb5b)
    
    (rebased-with-history from commit 07118a79ae94fcbb3de2d1778d751c228a64bac9)
    
    (rebased-with-history from commit 6064d4d94da30cc3dbbd0a8e4fa86c43d4057c83)
    
    (rebased-with-history from commit 203d86ac03acbd8b32a93d7c8432f9d25494c864)
    
    (rebased-with-history from commit 65d5600c13c691b2bd3f6ec81611bebce2ee21f3)
    
    (rebased-with-history from commit 4330289a59edd82f861c6aab499d68a218d258a1)
    
    (rebased-with-history from commit 0a4dc6f9fc08adbe5e705da8a1d46c013536cffb)
    
    (rebased-with-history from commit 3e5dd520e876ab22354cae57c2e1ae71cadb1931)
    
    (rebased-with-history from commit ca26f3d71055562fc05cbb79ce94ec83cc045c45)
    
    (rebased-with-history from commit a0ecf37002a05630a9c6b1d6cd025cd9b12e3b64)
    
    (rebased-with-history from commit a8a23b5c6fecea7a93e0b5a169f89930570ac2d7)
    
    (rebased-with-history from commit da11424877176ce05edf30635c5acf56f35e3b74)

---
+++ test/unit/org/apache/cassandra/service/ClientStateTest.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.service;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.apache.cassandra.SchemaLoader;
+import org.apache.cassandra.auth.*;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ClientStateTest
+{
+    @BeforeClass
+    public static void beforeClass()
+    {
+        System.setProperty("org.apache.cassandra.disable_mbean_registration", "true");
+        DatabaseDescriptor.daemonInitialization();
+    }
+    @AfterClass
+    public static void afterClass()
+    {
+        System.clearProperty("org.apache.cassandra.disable_mbean_registration");
+    }
+
+    @Test
+    public void permissionsCheckStartsAtHeadOfResourceChain() throws Exception {
+        // verify that when performing a permissions check, we start from the
+        // root IResource in the applicable hierarchy and proceed to the more
+        // granular resources until we find the required permission (or until
+        // we reach the end of the resource chain). This is because our typical
+        // usage is to grant blanket permissions on the root resources to users
+        // and so we save lookups, cache misses and cache space by traversing in
+        // this order. e.g. for DataResources, we typically grant perms on the
+        // 'data' resource, so when looking up a users perms on a specific table
+        // it makes sense to follow: data -> keyspace -> table
+
+        final AtomicInteger getPermissionsRequestCount = new AtomicInteger(0);
+        final IResource rootResource = DataResource.root();
+        final IResource tableResource = DataResource.table("test_ks", "test_table");
+        final AuthenticatedUser testUser = new AuthenticatedUser("test_user")
+        {
+            public Set<Permission> getPermissions(IResource resource)
+            {
+                getPermissionsRequestCount.incrementAndGet();
+                if (resource.equals(rootResource))
+                    return Permission.ALL;
+
+                fail(String.format("Permissions requested for unexpected resource %s", resource));
+                // need a return to make the compiler happy
+                return null;
+            }
+        };
+
+        // set up the role cache so that we can associate our test user with the client
+        // state (ClientState::login requires that the user's primary role has login privilege
+        Supplier<Map<RoleResource, Set<Role>>> bulkLoader =
+            () -> Collections.singletonMap(testUser.getPrimaryRole(),
+                                           Collections.singleton(new Role(testUser.getName(),
+                                                                          false,
+                                                                          true,
+                                                                          new HashMap<>(),
+                                                                          new HashSet<>())));
+        RolesCache cache = new RolesCache(new RoleTestUtils.LocalCassandraRoleManager(bulkLoader), () -> true);
+        cache.warm();
+        assertEquals(1, cache.getEstimatedSize());
+        Roles.initRolesCache(cache);
+
+        // finally, need to configure CassandraAuthorizer so we don't shortcircuit out of the authz process
+        DatabaseDescriptor.setAuthorizer(new CassandraAuthorizer());
+
+        // check permissions on the table, which should check for the root resource first
+        // & return successfully without needing to proceed further
+        ClientState state = ClientState.forInternalCalls();
+        state.login(testUser);
+        state.ensurePermission(Permission.SELECT, tableResource);
+        assertEquals(1, getPermissionsRequestCount.get());
+    }
+}
