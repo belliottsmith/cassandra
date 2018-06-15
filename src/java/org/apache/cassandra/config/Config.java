9c680cea964670667e39049422e7e15310d73cb6
Marcus Eriksson <marcus_eriksson@apple.com>
2018-06-14 20:44:40 -0700

    rdar://60088239 p35563981 OPS/PRF Compact biggest STCS bucket in L0
    CIE 3.0 Commits / Radars
    
    89daf758a6e147a67fa03b0363d47ebcf2405ff0 <rdar://problem/35563981>
    L0 compactions never recover when thousands unleveled
    Updated the way MockSchema generates to create one of each size.
    
    8943e148234eedc79c1e9938ebaf1e54308cb999 <rdar://problem/44820917>
    LCS logging too much during bootstrap with many tables
    
    CIE4 Review Commits
     35eafb70636f91375cee4c6518060a366ecf1527 review nit
    
    (rebased-with-history from commit 6d50ad83403ad0791b4812e0982caf8642492167)
    
    (rebased-with-history from commit 05d05297865089ed1549d020f0df1cacfe1dc1fb)
    
    (rebased-with-history from commit 43812687fd5f15e1a0de347b1b3da1bbba7ce26d)
    
    (rebased-with-history from commit a75b63ec8ae51aaceb55408d0e556935810835fb)
    
    (rebased-with-history from commit 2316206eb2c4bdf16d814b44ecdf5976c129c9da)
    
    (rebased-with-history from commit 340941e7d31a8b990a2bc222172ef3b990bdaa42)
    
    (rebased-with-history from commit c3df06c7f1a9ef3fb27d15a073d2f087d65b5f20)
    
    (rebased-with-history from commit 99984b7c49e2a12c0258d58f35f35492c090b34e)
    
    (rebased-with-history from commit 1e046066392b24796231a8b2ba4c0cea48f64cf1)
    
    (rebased-with-history from commit af9d803f86f1600fdd979a82435a14c0488a9639)
    
    (rebased-with-history from commit b641b780a5e52ec36f6d34f66f82cf1be72e27c1)
    
    (rebased-with-history from commit df45467390f39b97e06df524a1252cd47043c452)
    
    (rebased-with-history from commit 4ccbe8324e0b856db928b7f06fb814f851f915df)
    
    (rebased-with-history from commit 3cb144ed8db095dc5488dcdd034e1b9d51d9b6ef)
    
    (rebased-with-history from commit 3ee1de406e5d4eb3b16fd45f3aa0ba70bceb428c)
    
    (rebased-with-history from commit 767ce915976c1364b171f5ce48ba3e0a32eae0c5)
    
    (rebased-with-history from commit a306e455cb8f03b59777e89933674926cdbafa3e)

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -740,6 +740,8 @@ public class Config
      */
     public ConsistencyLevel denylist_consistency_level = ConsistencyLevel.QUORUM;
 
+    public volatile boolean compact_biggest_stcs_bucket_l0 = false;
+
     public Boolean disable_incremental_repair = Boolean.parseBoolean(System.getProperty("cassandra.disable_incremental_repair", "false"));
 
     public String full_query_log_dir = null; // CIE Cassandra 3.0 backward compatibility, create default FQL options if non-null.
