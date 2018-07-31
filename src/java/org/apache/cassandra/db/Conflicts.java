6889e8101dccfbb681de60491a8eb8bd18adc129
Benedict Elliott Smith <benedict@apple.com>
2018-07-31 15:40:53 +0100

    rework reconcile to consistently use 'return useLeft ? left : right' form of tests

--- src/java/org/apache/cassandra/db/Conflicts.java
+++ src/java/org/apache/cassandra/db/Conflicts.java
@@ -32,7 +32,7 @@ public abstract class Conflicts
         long leftTimestamp = left.timestamp();
         long rightTimestamp = right.timestamp();
         if (leftTimestamp != rightTimestamp)
-            return leftTimestamp < rightTimestamp ? right : left;
+            return leftTimestamp > rightTimestamp ? left : right;
 
         int leftLocalDeletionTime = left.localDeletionTime();
         int rightLocalDeletionTime = right.localDeletionTime();
@@ -40,45 +40,46 @@ public abstract class Conflicts
         boolean leftIsExpiringOrTombstone = leftLocalDeletionTime != Cell.NO_DELETION_TIME;
         boolean rightIsExpiringOrTombstone = rightLocalDeletionTime != Cell.NO_DELETION_TIME;
         if (leftIsExpiringOrTombstone != rightIsExpiringOrTombstone)
-            return leftIsExpiringOrTombstone ? right : left;
+            return leftIsExpiringOrTombstone ? left : right;
 
         ByteBuffer leftValue = left.value();
         ByteBuffer rightValue = right.value();
         int c = leftValue.compareTo(rightValue);
-        if (c < 0)
-            return right;
-        else if (c > 0)
+        if (c > 0)
             return left;
+        else if (c < 0)
+            return right;
 
         // Prefer the longest ttl if relevant
-        return leftLocalDeletionTime < rightLocalDeletionTime ? right : left;
+        return leftLocalDeletionTime > rightLocalDeletionTime ? left : right;
     }
 
     public static Cell resolveCounter(Cell left, Cell right)
     {
+        // No matter what the counter cell's timestamp is, a tombstone always takes precedence. See CASSANDRA-7346.
         boolean leftIsTombstone = left.isTombstone();
         boolean rightIsTombstone = right.isTombstone();
+        if (leftIsTombstone != rightIsTombstone)
+            return leftIsTombstone ? left : right;
+
         long leftTimestamp = left.timestamp();
         long rightTimestamp = right.timestamp();
-        // No matter what the counter cell's timestamp is, a tombstone always takes precedence. See CASSANDRA-7346.
-        if (leftIsTombstone)
-            // left is a tombstone: it has precedence over right if either right is not a tombstone, or left has a greater timestamp
-            return !rightIsTombstone || leftTimestamp > rightTimestamp ? left : right;
-
-        // If right is a tombstone, since left isn't one, it has precedence
-        if (rightIsTombstone)
-            return right;
+        if (leftIsTombstone) // ==> && rightIsTombstone
+            return leftTimestamp > rightTimestamp ? left : right;
 
         ByteBuffer leftValue = left.value();
         ByteBuffer rightValue = right.value();
         // Handle empty values. Counters can't truly have empty values, but we can have a counter cell that temporarily
         // has one on read if the column for the cell is not queried by the user due to the optimization of #10657. We
         // thus need to handle this (see #11726 too).
-        if (!leftValue.hasRemaining())
-            return rightValue.hasRemaining() || leftTimestamp > rightTimestamp ? left : right;
-
-        if (!rightValue.hasRemaining())
-            return right;
+        boolean leftIsEmpty = !leftValue.hasRemaining();
+        boolean rightIsEmpty = !rightValue.hasRemaining();
+        if (leftIsEmpty || rightIsEmpty)
+        {
+            if (leftIsEmpty != rightIsEmpty)
+                return leftIsEmpty ? left : right;
+            return leftTimestamp > rightTimestamp ? left : right;
+        }
 
         ByteBuffer merged = CounterContext.instance().merge(leftValue, rightValue);
         long timestamp = Math.max(leftTimestamp, rightTimestamp);
