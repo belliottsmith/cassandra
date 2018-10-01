903c04d872efc481e6c965312fe36d70a9ae011b
Alex Petrov <oleksandr.petrov@gmail.com>
2018-10-01 16:25:28 +0200

    Implement in-jvm distributed tests
    Fixes required to properly shutdown (most of the) threads
    
    Changes required for no-op MBean registration
    
    Separate loggers per instance
    
    Improve MBean wrapper / metric registration
    
    Enable circleci
    
    Add coordinator write tests
    
    Fix a problem with MBeans

--- test/unit/org/apache/cassandra/io/sstable/LegacySSTableTest.java
+++ test/unit/org/apache/cassandra/io/sstable/LegacySSTableTest.java
@@ -591,7 +591,7 @@ public class LegacySSTableTest
                  FileOutputStream os = new FileOutputStream(target);) {
                 while ((rd = is.read(buf)) >= 0)
                     os.write(buf, 0, rd);
-                }
+            }
         }
     }
 }
