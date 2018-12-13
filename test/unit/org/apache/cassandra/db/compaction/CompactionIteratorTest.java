d7c02b1b1f4aa16d9fce948aadc1e942c26ef4e9
Marcus Eriksson <marcuse@apache.org>
2018-12-13 14:31:54 +0100

    Make antiCompactGroup throw exception on failures and make anticompaction non cancellable again.
    Patch by marcuse; reviewed by Blake Eggleston for CASSANDRA-14936
    
    (cherry picked from commit 0a79f9f5c970dcb8265814cd5dc361eb2d4bec6b)

--- test/unit/org/apache/cassandra/db/compaction/CompactionIteratorTest.java
+++ test/unit/org/apache/cassandra/db/compaction/CompactionIteratorTest.java
@@ -21,14 +21,9 @@ import static org.junit.Assert.*;
 
 import java.util.*;
 
-import com.google.common.collect.*;
-
 import org.junit.Test;
 
-import org.apache.cassandra.config.CFMetaData;
 import org.apache.cassandra.cql3.CQLTester;
-import org.apache.cassandra.db.Keyspace;
-import org.apache.cassandra.db.partitions.AbstractUnfilteredPartitionIterator;
 import org.apache.cassandra.db.rows.*;
 import org.apache.cassandra.io.sstable.ISSTableScanner;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
@@ -104,4 +99,38 @@ public class CompactionIteratorTest extends CQLTester
             }
         }
     }
+
+    @Test
+    public void noTransformPartitionTest() throws Throwable
+    {
+        createTable("create table %s (id int, id2 int, d text, primary key (id, id2))");
+        for (int i = 0; i < 10; i++)
+        {
+            execute("insert into %s (id, id2, d) values (?, ?, 'abc')", i, i);
+        }
+        getCurrentColumnFamilyStore().forceBlockingFlush();
+        SSTableReader sstable = getCurrentColumnFamilyStore().getLiveSSTables().iterator().next();
+
+
+        try (CompactionController controller = new CompactionController(getCurrentColumnFamilyStore(), Integer.MAX_VALUE);
+             ISSTableScanner scanner = sstable.getScanner();
+             CompactionIterator iter = new CompactionIterator(OperationType.COMPACTION,
+                                                              Collections.singletonList(scanner),
+                                                              controller, FBUtilities.nowInSeconds(), null, null, false))
+        {
+            iter.stop();
+            int rowCnt = 0;
+            while (iter.hasNext())
+            {
+                iter.stop();
+                try (UnfilteredRowIterator rows = iter.next())
+                {
+                    rowCnt++;
+                }
+            }
+            assertEquals(10, rowCnt);
+
+        }
+    }
+
 }
---
+++ test/unit/org/apache/cassandra/repair/consistent/AbstractPendingAntiCompactionTest.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.repair.consistent;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+
+import com.google.common.collect.Sets;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+
+import org.apache.cassandra.SchemaLoader;
+import org.apache.cassandra.config.CFMetaData;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.config.Schema;
+import org.apache.cassandra.cql3.QueryProcessor;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.dht.Range;
+import org.apache.cassandra.dht.Token;
+import org.apache.cassandra.repair.AbstractRepairTest;
+import org.apache.cassandra.schema.KeyspaceParams;
+import org.apache.cassandra.service.ActiveRepairService;
+
+@Ignore
+public abstract class AbstractPendingAntiCompactionTest
+{
+    static final Collection<Range<Token>> FULL_RANGE;
+
+    static
+    {
+        Token minToken = DatabaseDescriptor.getPartitioner().getMinimumToken();
+        FULL_RANGE = Collections.singleton(new Range<>(minToken, minToken));
+    }
+
+    String ks;
+    final String tbl = "tbl";
+    final String tbl2 = "tbl2";
+
+    CFMetaData cfm;
+    ColumnFamilyStore cfs;
+    ColumnFamilyStore cfs2;
+
+    @BeforeClass
+    public static void setupClass()
+    {
+        SchemaLoader.prepareServer();
+        ActiveRepairService.instance.consistent.local.start();
+    }
+
+    @Before
+    public void setup()
+    {
+        ks = "ks_" + System.currentTimeMillis();
+        cfm = CFMetaData.compile(String.format("CREATE TABLE %s.%s (k INT PRIMARY KEY, v INT)", ks, tbl), ks);
+        CFMetaData cfm2 = CFMetaData.compile(String.format("CREATE TABLE %s.%s (k INT PRIMARY KEY, v INT)", ks, tbl2), ks);
+        SchemaLoader.createKeyspace(ks, KeyspaceParams.simple(1), cfm, cfm2);
+        cfs = Schema.instance.getColumnFamilyStoreInstance(cfm.cfId);
+        cfs2 = Schema.instance.getColumnFamilyStoreInstance(cfm2.cfId);
+    }
+
+    void makeSSTables(int num)
+    {
+        makeSSTables(num, cfs, 2);
+    }
+
+    void makeSSTables(int num, ColumnFamilyStore cfs, int rowsPerSSTable)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            int val = i * rowsPerSSTable;  // multiplied to prevent ranges from overlapping
+            for (int j = 0; j < rowsPerSSTable; j++)
+                QueryProcessor.executeInternal(String.format("INSERT INTO %s.%s (k, v) VALUES (?, ?)", ks, cfs.getTableName()), val + j, val + j);
+            cfs.forceBlockingFlush();
+        }
+        Assert.assertEquals(num, cfs.getLiveSSTables().size());
+    }
+
+    UUID prepareSession()
+    {
+        UUID sessionID = AbstractRepairTest.registerSession(cfs, true, true);
+        LocalSessionAccessor.prepareUnsafe(sessionID, AbstractRepairTest.COORDINATOR, Sets.newHashSet(AbstractRepairTest.COORDINATOR));
+        return sessionID;
+    }
+
+}
