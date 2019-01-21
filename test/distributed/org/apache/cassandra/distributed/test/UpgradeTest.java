ea706110126d17cc179b7f1d8aa0d9f7e19e0667
Benedict Elliott Smith <benedict@apache.org>
2019-01-21 18:49:39 +0000

    cleanup UpgradeTest

--- test/distributed/org/apache/cassandra/distributed/test/UpgradeTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/UpgradeTest.java
@@ -30,67 +30,39 @@ public class UpgradeTest extends DistributedTestBase
 {
 
     @Test
-    public void upgradeTest1() throws IOException
+    public void upgradeTest() throws IOException
     {
         Versions versions = Versions.find();
-        try (MultiVersionCluster cluster = init(MultiVersionCluster.create(3, versions.getLatest(Versions.Major.v30))))
+        final Versions.Major[][] tests = new Versions.Major[][] {
+            new Versions.Major[] { Versions.Major.v22, Versions.Major.v30, Versions.Major.v3X },
+            new Versions.Major[] { Versions.Major.v30, Versions.Major.v3X, Versions.Major.v4 }
+        };
+        for (Versions.Major[] test : tests)
         {
-            cluster.schemaChange("CREATE TABLE " + KEYSPACE + ".tbl (pk int, ck int, v int, PRIMARY KEY (pk, ck))");
-
-            cluster.get(1).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 1, 1)");
-            cluster.get(2).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 2, 2)");
-            cluster.get(3).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 3, 3)");
-
-//            for (Versions.Major major : new Versions.Major[] { Versions.Major.v30, Versions.Major.v3X, Versions.Major.v4 })
-            for (Versions.Major major : new Versions.Major[] { Versions.Major.v3X, Versions.Major.v4 })
-//            for (Versions.Major major : new Versions.Major[] { Versions.Major.v4 })
+            try (MultiVersionCluster cluster = init(MultiVersionCluster.create(3, versions.getLatest(test[0]))))
             {
-                for (int i = 1 ; i <= 3 ; ++i)
+                cluster.schemaChange("CREATE TABLE " + KEYSPACE + ".tbl (pk int, ck int, v int, PRIMARY KEY (pk, ck))");
+
+                cluster.get(1).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 1, 1)");
+                cluster.get(2).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 2, 2)");
+                cluster.get(3).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 3, 3)");
+
+                for (int m = 1 ; m < test.length ; ++m)
                 {
-                    cluster.get(i).shutdown();
-                    cluster.get(i).setVersion(versions.getLatest(major));
-                    cluster.get(i).startup();
+                    for (int i = 1 ; i <= 3 ; ++i)
+                    {
+                        cluster.get(i).shutdown();
+                        cluster.get(i).setVersion(versions.getLatest(test[m]));
+                        cluster.get(i).startup();
+                    }
+
+                    assertRows(cluster.coordinator(1).execute("SELECT * FROM " + KEYSPACE + ".tbl WHERE pk = ?",
+                                                              ConsistencyLevel.ALL,
+                                                              1),
+                               row(1, 1, 1),
+                               row(1, 2, 2),
+                               row(1, 3, 3));
                 }
-
-                assertRows(cluster.coordinator(1).execute("SELECT * FROM " + KEYSPACE + ".tbl WHERE pk = ?",
-                                                          ConsistencyLevel.ALL,
-                                                          1),
-                           row(1, 1, 1),
-                           row(1, 2, 2),
-                           row(1, 3, 3));
-            }
-        }
-    }
-
-    @Test
-    public void upgradeTest2() throws IOException
-    {
-        Versions versions = Versions.find();
-        try (MultiVersionCluster cluster = init(MultiVersionCluster.create(3, versions.getLatest(Versions.Major.v22))))
-        {
-            cluster.schemaChange("CREATE TABLE " + KEYSPACE + ".tbl (pk int, ck int, v int, PRIMARY KEY (pk, ck))");
-
-            cluster.get(1).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 1, 1)");
-            cluster.get(2).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 2, 2)");
-            cluster.get(3).executeInternal("INSERT INTO " + KEYSPACE + ".tbl (pk, ck, v) VALUES (1, 3, 3)");
-
-            for (Versions.Major major : new Versions.Major[] { Versions.Major.v30, Versions.Major.v3X })
-//            for (Versions.Major major : new Versions.Major[] { Versions.Major.v3X, Versions.Major.v4 })
-//            for (Versions.Major major : new Versions.Major[] { Versions.Major.v4 })
-            {
-                for (int i = 1 ; i <= 3 ; ++i)
-                {
-                    cluster.get(i).shutdown();
-                    cluster.get(i).setVersion(versions.getLatest(major));
-                    cluster.get(i).startup();
-                }
-
-                assertRows(cluster.coordinator(1).execute("SELECT * FROM " + KEYSPACE + ".tbl WHERE pk = ?",
-                                                          ConsistencyLevel.ALL,
-                                                          1),
-                           row(1, 1, 1),
-                           row(1, 2, 2),
-                           row(1, 3, 3));
             }
         }
     }
