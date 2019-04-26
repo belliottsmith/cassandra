de2014de80e85fcec38959c1e1339bfde23830a5
Blake Eggleston <beggleston@apple.com>
2019-04-26 14:15:22 -0700

    Fix mixed mode partition range scans with limit (#1251)
    Patch by Blake Eggleston; Reviewed by Sam Tunnicliffe for CASSANDRA-15072

--- src/java/org/apache/cassandra/db/ReadCommand.java
+++ src/java/org/apache/cassandra/db/ReadCommand.java
@@ -875,6 +875,8 @@ public abstract class ReadCommand implements ReadQuery
                 limits = DataLimits.distinctLimits(maxResults);
             else if (compositesToGroup == -1)
                 limits = DataLimits.thriftLimits(maxResults, perPartitionLimit);
+            else if (metadata.isStaticCompactTable())
+                limits = DataLimits.legacyCompactStaticCqlLimits(maxResults);
             else
                 limits = DataLimits.cqlLimits(maxResults);
 
