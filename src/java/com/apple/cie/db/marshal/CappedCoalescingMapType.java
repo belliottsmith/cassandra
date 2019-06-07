0736ba3f64435064c629a8a173720c26d7556dc8
Jon Meredith <jmeredithco@gmail.com>
2019-06-07 16:06:45 -0600

    rdar://60088349 p48562620 PRO/FTR Add CappedSortedMap and supporting functions
    7678e00fe05014871c110435fe8d677f2a51b00f  <rdar://problem/48562620>
    Add CappedSortedMap and supporting functions.
    
    29c2ccac3b8e9c3b779721d52eabb3646edc0a77  <rdar://problem/48562620>
                                              <rdar://problem/48523128>
    APNS ndeepq use case support functions, includes QuickTheories
    test for TQT / APNs
    
    e7a0b4f017cdee42b2c80fad4a6a89c5f6141394  <rdar://problem/51990128>
    Add a coalescing capped map to support APNs features
    
    31c1d4efba8e3ffd948d678d18a03d2fba1c6f5b  <rdar://problem/56171294>
    APNs koldestdeliverable/deliverable functions report incorrect type to client
    
    CIE4 Initial review commits
     4024ca23b29649eb341064eb9e2089f1ad9b6775 fixup: review nits from David
    
    (rebased-with-history from commit 3da9b9c2e8ef1af74368f0f6cd20ef97ff00c599)
    
    (rebased-with-history from commit 4dad689bcba325a47044871197785b787a1e87fa)
    
    (rebased-with-history from commit de0efc21d2fbd47d55ccc37a3cb077c687f22d46)
    
    (rebased-with-history from commit ffa9c34d206fbd37fe735e5093b210b0359be5aa)
    
    (rebased-with-history from commit 7a6dc823d892fd89735f8292d0be4c05be1e7131)
    
    (rebased-with-history from commit 27c5d78e7b973657513aca42abc70e53200b425a)
    
    (rebased-with-history from commit eb7a51cb5026fed7321068a3adb544ba44abac32)
    
    rdar://68102540 (3.0 backport rdar://68102268 (MapSerializer.getInstance does not include comparator in serializer cache, returning incorrect serializer))
    
    Introduce a way to create map serializers outside of the instances cache.
    The current cache does not check the comparator so it gets polluted when APNs registers bytesBytesMapSerializer.
    
    (rebased-with-history from commit 7376da0be6cd5f55a4df5712bd12fd83c014627c)
    
    (rebased-with-history from commit 3f31c144d0b93675ce65a241b4963816d5c60dea)
    
    (rebased-with-history from commit 36ca1d286feff06802ae770dc7c1fbfcbb276a4c)
    
    (rebased-with-history from commit a6bb979d2937b20e4284dad064824263b6b03cca)
    
    (rebased-with-history from commit 89d872f4b9b2af0621cd9fcebd227b5be99cce99)

---
+++ src/java/com/apple/cie/db/marshal/CappedCoalescingMapType.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.apple.cie.db.marshal;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.cassandra.cql3.CQL3Type;
+import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.db.marshal.BytesType;
+import org.apache.cassandra.db.marshal.FrozenType;
+import org.apache.cassandra.db.marshal.MapType;
+import org.apache.cassandra.db.marshal.ReversedType;
+import org.apache.cassandra.db.marshal.TimeUUIDType;
+import org.apache.cassandra.db.marshal.TupleType;
+import org.apache.cassandra.db.marshal.TypeParser;
+import org.apache.cassandra.db.rows.Cells;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.exceptions.SyntaxException;
+
+/**
+ * CappedCoalescingMapType supports storing a size-bounded collection of elements keyed by (reversed) timeuuid.  When
+ * the collection exceeds the size bound, the oldest elements are removed.
+ *
+ * The size bound is either stored with the elements in a supplied as a specially encoded tombstone
+ * cell withTimeUUID key value (created by {@literal com.apple.cie.cql3.functions.CappedCoalescingMapType#capFct}), or read
+ * from the system properties in {@literal com.apple.cie.db.marshal.cappedcoalescingmap.defaultcap}.
+ *
+ * CappedCoalescingMapType overrides {@literal wrapCellsBuilder} method to supply a custom cell builder that only
+ * passes through the size bound (N) if present, then the N most recent cells, AND extracts a coalescing key
+ * from the first element of the value tuple and ensures that only the most recent cell for each unique coalescing
+ * key is output.
+ *
+ * To create, use the custom type syntax
+ *
+ * <pre>
+ * CREATE TABLE tbl(pk int PRIMARY KEY, ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)');
+ * </pre>
+ *
+ * Add elements to the CCM using {@literal UPDATE} with {@code now()} as the current key.  The capacity
+ * can optionally be supplied using the {@literal cap} function.
+ *
+ * <pre>
+ * UPDATE tbl SET ccm[cap(:capacity)] = null, ccm[now()] = (:coalescingKey, :value) WHERE ...;
+ * </pre>
+ *
+ * Remove elements (before they are dropped from the end of the CCM) by updating their value to {@literal null}.
+ * If using per-instance capacity, it is important to supply a custom cap when deleting (otherwise
+ * older cap cells can be garbage collected used with TTL).
+ *
+ * <pre>
+ * UPDATE tbl SET ccm[cap(:capacity)] = null, ccm[:key] = null WHERE ...;
+ * </pre>
+ *
+ * @param <E> Element type
+ */
+public class CappedCoalescingMapType<E> extends CappedSortedMapType<ByteBuffer>
+{
+    final private AbstractType<E> elementType;
+
+    public CappedCoalescingMapType(AbstractType<E> elementType)
+    {
+        super(new TupleType(Arrays.asList(BytesType.instance, elementType)));
+        this.elementType = elementType;
+    }
+
+    private static <T> CappedCoalescingMapType<T> getCappedCoalescingMapInstance(AbstractType<T> elements)
+    {
+        return new CappedCoalescingMapType<>(elements);
+    }
+
+    @SuppressWarnings("unused")
+    public static CappedCoalescingMapType<?> getInstance(TypeParser parser) throws ConfigurationException, SyntaxException
+    {
+        List<AbstractType<?>> l = parser.getTypeParameters();
+
+        if (l.size() > 1)
+            throw new ConfigurationException("CappedCoalescingMapType takes exactly one type parameters");
+
+        return getCappedCoalescingMapInstance(l.get(0));
+    }
+
+    public Cells.Builder wrapCellsBuilder(Cells.Builder builder)
+    {
+        return CappedCoalescingMapCellsResolver.instance.wrapCellsBuilder(builder, (TupleType) getValuesType());
+    }
+
+    @Override
+    public CQL3Type asCQL3Type()
+    {
+        return new CQL3Type.Custom(this);
+    }
+
+    @Override
+    public String toString(boolean ignoreFreezing)
+    {
+        boolean includeFrozenType = !ignoreFreezing && !isMultiCell();
+
+        StringBuilder sb = new StringBuilder();
+        if (includeFrozenType)
+        {
+            sb.append(FrozenType.class.getName())
+              .append('(');
+        }
+        sb.append(getClass().getName())
+          .append(TypeParser.stringifyTypeParameters(Collections.singletonList(elementType), ignoreFreezing || !isMultiCell()));
+        if (includeFrozenType)
+            sb.append(')');
+        return sb.toString();
+    }
+}
