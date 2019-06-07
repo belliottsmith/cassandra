7c43e3f0afedc39b0c545b9b109d4f79dfe0afa2
Jon Meredith <jmeredithco@gmail.com>
2019-06-07 16:06:45 -0600

    rdar://60088349 p48562620 PRO/FTR Add CappedSortedMap and supporting functions
    7678e00fe05014871c110435fe8d677f2a51b00f  <rdar://problem/48562620>
    Add CappedSortedMap and supporting functions.
    
    29c2ccac3b8e9c3b779721d52eabb3646edc0a77  <rdar://problem/48562620>
                                              <rdar://problem/48523128>
    APNS ndeepq use case support functions, includes QuickTheories
    test for TQT / APNs
    
    e7a0b4f017cdee42b2c80fad4a6a89c5f6141394  <rdar://problem/51990128>
    Add a coalescing capped map to support APNs features
    
    31c1d4efba8e3ffd948d678d18a03d2fba1c6f5b  <rdar://problem/56171294>
    APNs koldestdeliverable/deliverable functions report incorrect type to client
    
    CIE4 Initial review commits
     4024ca23b29649eb341064eb9e2089f1ad9b6775 fixup: review nits from David
    
    (rebased-with-history from commit 3da9b9c2e8ef1af74368f0f6cd20ef97ff00c599)
    
    (rebased-with-history from commit 4dad689bcba325a47044871197785b787a1e87fa)
    
    (rebased-with-history from commit de0efc21d2fbd47d55ccc37a3cb077c687f22d46)
    
    (rebased-with-history from commit ffa9c34d206fbd37fe735e5093b210b0359be5aa)
    
    (rebased-with-history from commit 7a6dc823d892fd89735f8292d0be4c05be1e7131)
    
    (rebased-with-history from commit 27c5d78e7b973657513aca42abc70e53200b425a)
    
    (rebased-with-history from commit eb7a51cb5026fed7321068a3adb544ba44abac32)
    
    rdar://68102540 (3.0 backport rdar://68102268 (MapSerializer.getInstance does not include comparator in serializer cache, returning incorrect serializer))
    
    Introduce a way to create map serializers outside of the instances cache.
    The current cache does not check the comparator so it gets polluted when APNs registers bytesBytesMapSerializer.
    
    (rebased-with-history from commit 7376da0be6cd5f55a4df5712bd12fd83c014627c)
    
    (rebased-with-history from commit 3f31c144d0b93675ce65a241b4963816d5c60dea)
    
    (rebased-with-history from commit 36ca1d286feff06802ae770dc7c1fbfcbb276a4c)
    
    (rebased-with-history from commit a6bb979d2937b20e4284dad064824263b6b03cca)
    
    (rebased-with-history from commit 89d872f4b9b2af0621cd9fcebd227b5be99cce99)
    
    (rebased-with-history from commit 0736ba3f64435064c629a8a173720c26d7556dc8)
    
    (rebased-with-history from commit abca5cffc4d89c1e4521651f5b2aaacf07225c30)
    
    rdar://72042385 (Empty partition is written to SSTable when APNS capped collection is used with TTL)
    
    (rebased-with-history from commit 7af7f1c0c47f998fa827ced1cd5e8ff188510d55)
    
    (rebased-with-history from commit 73dd37f2e715b70fed43c5ac8dac1f3e86eb2926)
    
    (rebased-with-history from commit 28395230dd373b95066f1920679ac34678770a72)
    
    (rebased-with-history from commit 6681e9abdaffe0f7639c7cbf1b93c59c93a00191)
    
    (rebased-with-history from commit f9e027be12347ab1999e98a1bbb30cf39f517145)
    
    (rebased-with-history from commit f7b04c67be55489630cd7432dbe96ae2d168d9ca)
    
    rdar://59391677 (Fix flaky test com.apple.cie.cql3.functions.ApnsCoalescingMapQtTest qtTest) (#2964)
    
    Model was incorrectly handling ACKs.  After being acked, the coalescing key is no longer
    available and the tombstone will count towards the number of active cells, even if another
    cell with that coallescing key exists.

---
+++ src/java/com/apple/cie/cql3/functions/CappedSortedMap.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2018-2019 Apple, Inc. All rights reserved.
+ */
+package com.apple.cie.cql3.functions;
+
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import com.google.common.collect.ImmutableList;
+
+import org.apache.cassandra.cql3.functions.Function;
+import org.apache.cassandra.cql3.functions.NativeScalarFunction;
+import org.apache.cassandra.db.marshal.Int32Type;
+import org.apache.cassandra.db.marshal.TimeUUIDType;
+import org.apache.cassandra.transport.ProtocolVersion;
+import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.cassandra.utils.TimeUUID;
+
+public abstract class CappedSortedMap
+{
+    public  static final int CAP_MAX = (1 << 14) - 1;
+    private static final long CAP_MASK = (long) CAP_MAX;
+    private static final long MSB = makeMSB();
+
+    /* From RFC4122 section 4.1.2
+     *
+     * The capacity is encoded in a version 1, variant 2 UUID
+     * as 14-bits in the clock sequence field, with the timestamp set to the
+     * maximum permissible time.
+     *
+     *       0                   1                   2                   3
+     *    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+     *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     *   |                          time_low                             |
+     *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     *   |       time_mid                |         time_hi_and_version   |
+     *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     *   |clk_seq_hi_res |  clk_seq_low  |         node (0-1)            |
+     *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     *   |                         node (2-5)                            |
+     *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     */
+    private static long makeMSB()
+    {
+        long timestamp = 1152921504606846975L; // 0x0FFFFFFFFFFFFFFF
+        long msb = 0L;
+        msb |= (4294967295L & timestamp) << 32;           // 0x00000000FFFFFFFF (31- 0 timestamp) = TTTTTTTT00000000
+        msb |= (281470681743360L & timestamp) >>> 16;     // 0x0000FFFF00000000 (47-32 timestamp) = 00000000UUUU0000
+        msb |= (1152640029630136320L & timestamp) >>> 48; // 0x0FFF000000000000 (62-48 timestamp) = 0000000000000VVV
+        msb |= 4096L;                                     // 0x0000000000001000 (version)
+        return msb;                                       //                                      = TTTTTTTTUUUU1VVV
+    }
+
+    private static long makeLSB(int cap)
+    {
+        long truncCap = Math.min(cap, CAP_MAX);
+        long lsb = 281474976710655L;          // node    0x0000FFFFFFFFFFFF
+        lsb |= (truncCap & CAP_MASK) << 48;   // cap     0x00QQ000000000000 (14-bits of capacity)
+        lsb |= -9223372036854775808L;         // variant 0x8000000000000000
+        return lsb;
+    }
+
+    /**
+     * Make a special cap TimeUUID for use by @{link com.apple.cie.db.marshal.CappedSortedMapType}.
+     */
+    static public TimeUUID withCap(int length)
+    {
+        return TimeUUID.fromBytes(MSB, makeLSB(length));
+    }
+
+    static public int getCap(TimeUUID uuid)
+    {
+        if (uuid.msb() != MSB)
+            return -1;
+        long lsb = uuid.lsb();
+        return (int) ((lsb >> 48) & CAP_MASK);
+    }
+
+    private static final Function capFct = new NativeScalarFunction("cap", TimeUUIDType.instance, Int32Type.instance)
+    {
+        public ByteBuffer execute(ProtocolVersion protocolVersion, List<ByteBuffer> parameters)
+        {
+            int len = Int32Type.instance.compose(parameters.get(0));
+            return withCap(len).toBytes();
+        }
+    };
+
+    @SuppressWarnings("unused")
+    public static Collection<Function> all()
+    {
+        return ImmutableList.of(capFct);
+    }
+}
---
+++ test/unit/com/apple/cie/db/marshal/CappedCoalescingMapTypeTest.java
@@ -0,0 +1,415 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.cie.db.marshal;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.apple.cie.cql3.functions.CappedSortedMap;
+import com.datastax.driver.core.utils.UUIDs;
+import org.apache.cassandra.cql3.CQLTester;
+import org.apache.cassandra.db.marshal.Int32Type;
+import org.apache.cassandra.db.marshal.TupleType;
+import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.cassandra.utils.TimeUUID;
+
+import static org.junit.Assert.assertEquals;
+
+public class CappedCoalescingMapTypeTest extends CQLTester
+{
+    static ByteBuffer makeCoalescingKey(int key)
+    {
+        return  ByteBufferUtil.bytes(key); // just make a blob out of the text representation of the int
+    }
+
+    static private TimeUUID makeMessageId(int msgIdx)
+    {
+        final long start = 0L;
+        return TimeUUID.fromUuid(UUIDs.startOf(start + msgIdx));
+    }
+
+    static ByteBuffer makeCoalescingKeyAndValue(int key, int value)
+    {
+        return TupleType.buildValue(new ByteBuffer[] {makeCoalescingKey(key), Int32Type.instance.decompose(value) });
+    }
+
+    @Test
+    public void basicFunctionality() throws Throwable
+    {
+        createTable("CREATE TABLE %s (pk int, ck int, ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)', PRIMARY KEY (pk, ck))");
+
+        assertRows(execute(String.format("SELECT type FROM system_schema.columns WHERE keyspace_name = '%s' AND table_name = '%s' AND column_name = 'ccm'", KEYSPACE, currentTable())),
+                   row("'com.apple.cie.db.marshal.CappedCoalescingMapType(org.apache.cassandra.db.marshal.Int32Type)'"));
+
+        int pk = 1;
+        int ck = 1;
+        final TimeUUID cap = CappedSortedMap.withCap(3);
+        for (int msgIdx = 1; msgIdx <= 5; msgIdx++)
+        {
+            TimeUUID messageId = makeMessageId(msgIdx);
+            ByteBuffer coalescingKey = makeCoalescingKey(msgIdx % 2);
+            execute("UPDATE %s SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                    cap, messageId, coalescingKey, msgIdx, pk, ck);
+        }
+
+        assertRows(execute("SELECT pk, ck, ccm FROM %s"),
+                   row(pk, ck,
+                       map(
+                       makeMessageId(4), makeCoalescingKeyAndValue(0, 4), // coalescing key 0, most recent is 4
+                       makeMessageId(5), makeCoalescingKeyAndValue(1, 5)) // coalescing key 1, most recent is 5
+                   ));
+    }
+
+
+    @Test
+    public void varyCap() throws Throwable
+    {
+        createTable("CREATE TABLE %s (pk int, ck int, ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)', PRIMARY KEY (pk, ck))");
+
+        assertRows(execute(String.format("SELECT type FROM system_schema.columns WHERE keyspace_name = '%s' AND table_name = '%s' AND column_name = 'ccm'", KEYSPACE, currentTable())),
+                   row("'com.apple.cie.db.marshal.CappedCoalescingMapType(org.apache.cassandra.db.marshal.Int32Type)'"));
+
+        int pk = 1;
+        int ck = 1;
+        final TimeUUID cap1 = CappedSortedMap.withCap(1);
+        final TimeUUID cap2 = CappedSortedMap.withCap(2);
+        final TimeUUID cap4 = CappedSortedMap.withCap(4);
+
+        // Write three messages with cap=2
+        int msgIdx = 1;
+        while (msgIdx <= 3)
+        {
+            execute("UPDATE %s SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                    cap2, makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            msgIdx++;
+        }
+
+        assertRows(execute("SELECT pk, ck, ccm FROM %s"),
+                   row(pk, ck,
+                       map(
+                       makeMessageId(3), makeCoalescingKeyAndValue(300, 3),
+                       makeMessageId(2), makeCoalescingKeyAndValue(200, 2))
+                   ));
+
+        // Write one message with cap=1, should be the last
+        while (msgIdx <= 4)
+        {
+            execute("UPDATE %s SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                    cap1, makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            msgIdx++;
+        }
+
+        assertRows(execute("SELECT pk, ck, ccm FROM %s"),
+                   row(pk, ck,
+                       map(
+                       makeMessageId(4), makeCoalescingKeyAndValue(400, 4))
+                   ));
+
+        // Increase back up to 4 and see what we get
+        while (msgIdx <= 5)
+        {
+            execute("UPDATE %s SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                    cap4, makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            msgIdx++;
+        }
+
+        // As the ccm is (probably) still in the memtable, the items pushed out of the end
+        // of the ccm have been dropped before making it to an sstable, so increasing the
+        // cap will only return the existing values.
+        assertRows(execute("SELECT pk, ck, ccm FROM %s"),
+                   row(pk, ck,
+                       map(
+                       makeMessageId(4), makeCoalescingKeyAndValue(400, 4),
+                       makeMessageId(5), makeCoalescingKeyAndValue(500, 5))
+                   ));
+
+        // Write 3 more to push msgIdx 4 out
+        while (msgIdx <= 8)
+        {
+            execute("UPDATE %s SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                    cap4, makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            msgIdx++;
+        }
+        assertRows(execute("SELECT pk, ck, ccm FROM %s"),
+                   row(pk, ck,
+                       map(
+                       makeMessageId(5), makeCoalescingKeyAndValue(500, 5),
+                       makeMessageId(6), makeCoalescingKeyAndValue(600, 6),
+                       makeMessageId(7), makeCoalescingKeyAndValue(700, 7),
+                       makeMessageId(8), makeCoalescingKeyAndValue(800, 8))
+                   ));
+    }
+
+    @Test
+    public void adjustHotProperties() throws Throwable
+    {
+        int defaultCap = CappedCoalescingMapCellsResolver.instance.getDefaultCap();
+        int minEffectiveCap = CappedCoalescingMapCellsResolver.instance.getMinEffectiveCap();
+        int maxEffectiveCap = CappedCoalescingMapCellsResolver.instance.getMaxEffectiveCap();
+
+        try
+        {
+            // Check the minimum effective cap can only be set in bounds
+            CappedCoalescingMapCellsResolver.instance.setMinEffectiveCap(-1);
+            assertEquals(0, CappedCoalescingMapCellsResolver.instance.getMinEffectiveCap());
+            CappedCoalescingMapCellsResolver.instance.setMinEffectiveCap(CappedSortedMap.CAP_MAX + 1);
+            assertEquals(CappedSortedMap.CAP_MAX, CappedCoalescingMapCellsResolver.instance.getMinEffectiveCap());
+
+            // Check the Maximum effective cap can only be set in bounds
+            // and that the minimum drops to be less than or equal the max
+            CappedCoalescingMapCellsResolver.instance.setMaxEffectiveCap(CappedSortedMap.CAP_MAX + 1);
+            assertEquals(CappedSortedMap.CAP_MAX, CappedCoalescingMapCellsResolver.instance.getMaxEffectiveCap());
+            assertEquals(CappedSortedMap.CAP_MAX, CappedCoalescingMapCellsResolver.instance.getMinEffectiveCap());
+
+            CappedCoalescingMapCellsResolver.instance.setMaxEffectiveCap(-1);
+            assertEquals(0, CappedCoalescingMapCellsResolver.instance.getMaxEffectiveCap());
+            assertEquals(0, CappedCoalescingMapCellsResolver.instance.getMinEffectiveCap());
+
+            CappedCoalescingMapCellsResolver.instance.setMinEffectiveCap(0);
+            CappedCoalescingMapCellsResolver.instance.setMaxEffectiveCap(CappedSortedMap.CAP_MAX);
+
+            createTable("CREATE TABLE %s (pk int, ck int, " +
+                        "ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)', PRIMARY KEY (pk, ck))" +
+                        " WITH gc_grace_seconds = 0");
+
+            // Reduce the default cap to 2
+            CappedCoalescingMapCellsResolver.instance.setDefaultCap(2);
+            assertEquals(2, CappedCoalescingMapCellsResolver.instance.getDefaultCap());
+
+            // Write three entries to a ccm limited to two
+            final int pk = 1;
+            final int ck = 1;
+            for (int msgIdx = 1; msgIdx <= 3; msgIdx++)
+            {
+                execute("UPDATE %s SET ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                        makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            }
+            assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                       row(map(makeMessageId(2), makeCoalescingKeyAndValue(200, 2),
+                               makeMessageId(3), makeCoalescingKeyAndValue(300, 3))));
+
+            assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                       row(map(makeMessageId(2), makeCoalescingKeyAndValue(200, 2),
+                               makeMessageId(3), makeCoalescingKeyAndValue(300, 3))));
+
+            // Lower the effective limit to 1 and check result is limited.
+            flush(); // otherwise CCM served out of Memtable
+            CappedCoalescingMapCellsResolver.instance.setMaxEffectiveCap(1);
+            assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                       row(map(makeMessageId(3), makeCoalescingKeyAndValue(300, 3))));
+
+            // Raise again and see the default limit is back in place
+            CappedCoalescingMapCellsResolver.instance.setMaxEffectiveCap(CappedSortedMap.CAP_MAX);
+
+            assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                       row(map(makeMessageId(2), makeCoalescingKeyAndValue(200, 2),
+                               makeMessageId(3), makeCoalescingKeyAndValue(300, 3))));
+        }
+        finally
+        {
+            CappedCoalescingMapCellsResolver.instance.setDefaultCap(defaultCap);
+            CappedCoalescingMapCellsResolver.instance.setMinEffectiveCap(minEffectiveCap);
+            CappedCoalescingMapCellsResolver.instance.setMaxEffectiveCap(maxEffectiveCap);
+        }
+    }
+
+    @Test
+    public void disableCellsResolver() throws Throwable
+    {
+        try
+        {
+            CappedCoalescingMapCellsResolver.instance.setCellResolverEnabled(true);
+            createTable("CREATE TABLE %s (pk int, ck int, " +
+                        "ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)', PRIMARY KEY (pk, ck))" +
+                        " WITH gc_grace_seconds = 0");
+
+            final int pk = 1;
+            final int ck = 1;
+            final int cap = 2;
+
+            // Write three entries to a ccm limited to two
+            for (int msgIdx = 1; msgIdx <= 3; msgIdx++)
+            {
+                execute("UPDATE %s SET ccm[cap(?)] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                        cap, makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            }
+            assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                       row(map(makeMessageId(2), makeCoalescingKeyAndValue(200, 2),
+                               makeMessageId(3), makeCoalescingKeyAndValue(300, 3))));
+
+            CappedCoalescingMapCellsResolver.instance.setCellResolverEnabled(false);
+
+            // Write three more entries to a ccm limited to two
+            for (int msgIdx = 4; msgIdx <= 6; msgIdx++)
+            {
+                execute("UPDATE %s SET ccm[cap(?)] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                        cap, makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            }
+            assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                       row(map(makeMessageId(2), makeCoalescingKeyAndValue(200, 2),
+                               makeMessageId(3), makeCoalescingKeyAndValue(300, 3),
+                               makeMessageId(4), makeCoalescingKeyAndValue(400, 4),
+                               makeMessageId(5), makeCoalescingKeyAndValue(500, 5),
+                               makeMessageId(6), makeCoalescingKeyAndValue(600, 6))));
+
+            // Reenable and check previously set cap respected
+            CappedCoalescingMapCellsResolver.instance.setCellResolverEnabled(true);
+
+            for (int msgIdx = 7; msgIdx <=7; msgIdx++)
+            {
+                execute("UPDATE %s SET ccm[cap(?)] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ?",
+                        cap, makeMessageId(msgIdx), makeCoalescingKey(msgIdx * 100), msgIdx, pk, ck);
+            }
+            assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                       row(map(makeMessageId(6), makeCoalescingKeyAndValue(600, 6),
+                               makeMessageId(7), makeCoalescingKeyAndValue(700, 7))));
+        }
+        finally
+        {
+            CappedCoalescingMapCellsResolver.instance.setCellResolverEnabled(true);
+        }
+    }
+
+    @Test
+    public void emptyPartitionExpirationNonPurged() throws Throwable
+    {
+        int pk = 1;
+        int ck = 1;
+
+        emptyPartitionExpirationSetup(pk, ck);
+        Thread.sleep(6_000); // expire but don't purge tombstone
+        assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck));
+        compact();
+        Assert.assertEquals(1, getCurrentColumnFamilyStore().getLiveSSTables().size());
+        compact();
+        Assert.assertEquals(0, getCurrentColumnFamilyStore().getLiveSSTables().size());
+    }
+
+    @Test
+    public void emptyPartitionExpirationPurged() throws Throwable
+    {
+        int pk = 1;
+        int ck = 1;
+
+        emptyPartitionExpirationSetup(pk, ck);
+        Thread.sleep(11_000); // 5 seconds of TTL + 5 seconds of GC Grace
+        assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck));
+        compact();
+        Assert.assertEquals(0, getCurrentColumnFamilyStore().getLiveSSTables().size());
+        compact();
+        Assert.assertEquals(0, getCurrentColumnFamilyStore().getLiveSSTables().size());
+    }
+
+    public void emptyPartitionExpirationSetup(int pk, int ck) throws Throwable
+    {
+        createTable("CREATE TABLE %s (pk int, ck int, ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)', PRIMARY KEY (pk, ck))" +
+                    " WITH gc_grace_seconds = 5");
+
+        final TimeUUID cap = CappedSortedMap.withCap(3);
+        for (int msgIdx = 1; msgIdx <= 5; msgIdx++)
+        {
+            TimeUUID messageId = makeMessageId(msgIdx);
+            ByteBuffer coalescingKey = makeCoalescingKey(msgIdx % 2);
+            execute("UPDATE %s USING TTL 5 SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ? ",
+                    cap, messageId, coalescingKey, msgIdx, pk, ck);
+            flush();
+        }
+    }
+
+    @Test
+    public void mixedExpiredAndAliveRows() throws Throwable
+    {
+        createTable("CREATE TABLE %s (pk int, ck int, ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)', " +
+                    "PRIMARY KEY (pk, ck))");
+
+        int pk = 1;
+        int ck = 1;
+        final TimeUUID cap = CappedSortedMap.withCap(3);
+
+        write(pk, ck, cap, 1, 1, 5);
+        flush();
+        write(pk, ck, cap, 2, 0, 10);
+        flush();
+        Thread.sleep(6_000);
+        compact();
+
+        assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                   row(map(makeMessageId(2), makeCoalescingKeyAndValue(0, 2))));
+
+        compact();
+        Assert.assertEquals(1, getCurrentColumnFamilyStore().getLiveSSTables().size());
+    }
+
+    @Test
+    public void mixedTtlAndAliveRows() throws Throwable
+    {
+        createTable("CREATE TABLE %s (pk int, ck int, ccm 'com.apple.cie.db.marshal.CappedCoalescingMapType(Int32Type)', " +
+                    "PRIMARY KEY (pk, ck)) WITH gc_grace_seconds = 5");
+
+        int pk = 1;
+        int ck = 1;
+        final TimeUUID cap = CappedSortedMap.withCap(3);
+
+        // Write one non-expiring cell
+        write(pk, ck, cap, 1, 1, 0);
+        flush();
+        write(pk, ck, cap, 2, 2, 5);
+        write(pk, ck, cap, 3, 3, 5);
+        write(pk, ck, cap, 4, 4, 5);
+        flush();
+        compact();
+
+        // Non-expiring cell is kicked out by the later writes
+        assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck),
+                   row(map(makeMessageId(2), makeCoalescingKeyAndValue(2, 2),
+                           makeMessageId(3), makeCoalescingKeyAndValue(3, 3),
+                           makeMessageId(4), makeCoalescingKeyAndValue(4, 4))));
+
+        // After compaction, we have one sstable, where cap cell has MAX_DELETION_TIME ldt
+        Assert.assertEquals(1, getCurrentColumnFamilyStore().getLiveSSTables().size());
+        Thread.sleep(11_000);
+        compact();
+
+        // All cells are now expired
+        assertRows(execute("SELECT ccm FROM %s WHERE pk = ? AND ck = ?", pk, ck));
+
+        // Now, cell is compacted away
+        Assert.assertEquals(0, getCurrentColumnFamilyStore().getLiveSSTables().size());
+    }
+
+
+    public void write(int pk, int ck, TimeUUID cap, int msgIdx, int msgCoalescingIdx, int ttl) throws Throwable
+    {
+        TimeUUID messageId = makeMessageId(msgIdx);
+        ByteBuffer coalescingKey = makeCoalescingKey(msgCoalescingIdx);
+        if (ttl > 0)
+        {
+            execute("UPDATE %s USING TTL ? SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ? ",
+                    ttl, cap, messageId, coalescingKey, msgIdx, pk, ck);
+        }
+        else
+        {
+            execute("UPDATE %s SET ccm[?] = null, ccm[?] = (?, ?) WHERE pk = ? AND ck = ? ",
+                    cap, messageId, coalescingKey, msgIdx, pk, ck);
+
+        }
+    }
+}
