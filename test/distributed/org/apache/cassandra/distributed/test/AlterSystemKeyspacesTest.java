8551465541abacd08821e6e9c1c29fb95dc5de57
Aleksey Yeshchenko <aleksey@apache.org>
2019-10-24 14:19:27 +0100

    rdar://60088267 p56247937 PRO/FTR Permit altering tables in cie_internal keyspaces
    CIE 3.0 Commit / Radars
    1afd1cd779e7e56af193b426626d3dc4d9075a48 <rdar://56247937>
    Permit altering tables in cie_internal and system_auth keyspaces
    
    Support for altering system_auth has been dropped.
    
    CIE4 Inital review commits
     217716157a fixup: remove ks_quota from alter table schema tests - not created yet.
     4668fade5e fixup: remove system_auth keyspaces from alter system keyspace test.
    
    (rebased-with-history from commit 846375eb54dbcea412bc225aa43b439ee471499c)
    
    (rebased-with-history from commit b6f4e6ba2964d4e9bb579d6a3ae744c5c7aaa322)
    
    (rebased-with-history from commit a72418bb229e8c2759698b26c5b39a9c251c24f8)
    
    (rebased-with-history from commit 6dccb7d411c2c80656d694d4282fa90b8b746146)
    
    (rebased-with-history from commit 815f738be4535d7a7cf9014e42b2cefceddd96e7)
    
    (rebased-with-history from commit 23032ec41983535b04649fb94528a0065752fc52)
    
    (rebased-with-history from commit 7863121366510ae23befde855716ad3ce675d62b)
    
    (rebased-with-history from commit 10de4f3e165ac7581b5e3f1fd7ecfa04ef491afa)
    
    (rebased-with-history from commit d67380fc0ac62a217524a9d7fdc511eb67509b45)
    
    (rebased-with-history from commit 34c37de755e607ecc7b45ccc8295b40ed9b61632)
    
    (rebased-with-history from commit 606efcc3b0d2e246e966487445bf3bd9a7d9cac1)
    
    (rebased-with-history from commit 85006ee6b5d10838598b053f64b7ff4e76ef5809)
    
    (rebased-with-history from commit 0e6c88112474930327bd1dcde25abaf96684ac5e)
    
    (rebased-with-history from commit bea3203fdcfccfb25d3c93aa834d193705e624aa)
    
    (rebased-with-history from commit 6a54d9bbf9de709fa0f2067919809da804e8f043)
    
    (rebased-with-history from commit d892a7d323e9799f1bbf96e5bef5a772a601e176)

---
+++ test/distributed/org/apache/cassandra/distributed/test/AlterSystemKeyspacesTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cassandra.distributed.test;
+
+import org.junit.Test;
+
+import org.apache.cassandra.distributed.api.ConsistencyLevel;
+import org.apache.cassandra.distributed.api.Feature;
+import org.apache.cassandra.distributed.api.ICluster;
+import org.apache.cassandra.distributed.api.ICoordinator;
+
+public class AlterSystemKeyspacesTest extends TestBaseImpl
+{
+    /**
+     * See rdar://56247937 (Permit altering tables in cie_internal and system_auth keyspaces).
+     */
+    @Test
+    public void alterCIEInternalTablesTest() throws Throwable
+    {
+        try (ICluster cluster = init(builder().withNodes(1).withConfig(c -> c.with(Feature.GOSSIP)).start()))
+        {
+            ICoordinator coordinator = cluster.coordinator(1);
+
+            coordinator.execute("ALTER TABLE cie_internal.schema_drop_log " +
+                                "WITH comment = 'Store all dropped tables for apple internal patch' " +
+                                "AND gc_grace_seconds = 864000 " +
+                                "AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true'};",
+                                ConsistencyLevel.ONE);
+        }
+    }
+}
