7f355c8a41bb37859f23cf401243d285d97eeab9
Sam Tunnicliffe <samt@apple.com>
2019-11-11 19:12:06 +0000

    Disable creation of materialized views by default
    <rdar://problem/57075746> Materialized Views should be disabled by default in 3.0

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -396,7 +396,7 @@ public class Config
     public boolean enable_user_defined_functions = false;
     public boolean enable_scripted_user_defined_functions = false;
 
-    public boolean enable_materialized_views = true;
+    public boolean enable_materialized_views = false;
 
     /**
      * Optionally disable asynchronous UDF execution.
