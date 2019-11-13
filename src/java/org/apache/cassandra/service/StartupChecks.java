90d36e3590012086010458f935b1a2b860007bdf
Hari Nukala <hnukala@apple.com>
2019-11-14 00:28:22 +0530

    Adding Amazon's Corretto crypto provider (#1372)
    * <rdar://problem/56258571> Amazon Corretto Crypto Provider (ACCP) with cie-cassandra

--- src/java/org/apache/cassandra/service/StartupChecks.java
+++ src/java/org/apache/cassandra/service/StartupChecks.java
@@ -24,6 +24,8 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.security.Provider;
+import java.security.Security;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -83,6 +85,7 @@ public class StartupChecks
                                                                       checkValidLaunchDate,
                                                                       checkJMXPorts,
                                                                       inspectJvmOptions,
+                                                                      inspectDefaultSecurityProvider,
                                                                       checkNativeLibraryInitialization,
                                                                       initSigarLibrary,
                                                                       checkMaxMapCount,
@@ -235,6 +238,17 @@ public class StartupChecks
                         return true;
             }
             return false;
+        }
+    };
+
+    public static final StartupCheck inspectDefaultSecurityProvider = new StartupCheck()
+    {
+        public void execute()
+        {
+            Provider[] providers = Security.getProviders();
+            for (Provider p : providers)
+                logger.info("Detected security provider: " + p);
+
         }
     };
 
