dc2d41e7c2b2c1e084eaa1795f706c88978e8997
Jon Meredith <jmeredithco@apple.com>
2020-03-30 13:51:40 -0600

    rdar://61116767 p21020076 ENV/BLD Capture test logs in Rio/Jenkins
    Introduces a timeout for the parallelci workers so the parallelci
    command will terminate before the Rio build timeout and still
    produce logging output from the completed tasks.
    
    Change Rio to pick up the Junit XML reports and test logs
    directly rather than copying them into the older .out directory
    it expects.
    
    Trim the length of the Rio labels applied to K8S as some pipeline
    names can get longer than the max of 63 characters.

--- rio/parallel-tests-step.sh
+++ rio/parallel-tests-step.sh
@@ -7,29 +7,19 @@ set -o xtrace
 
 readonly RIO_DIR="$(dirname "$0")"
 readonly BASE_DIR="${RIO_DIR}/.."
+readonly PARALLELOUTPUT="${PWD}/parallel-output"
 
-cleanup() {
-    if [ -d "${BASE_DIR}/build/test/output" ]; then
-        echo "Copying ParallelCI output for Rio to pass on to Quality"
-        mkdir -p "${BASE_DIR}/.out/test-results"
-        find "${BASE_DIR}/build/test/output" -name '*.xml' | while read -r xml
-        do
-            cp "$xml" .out/test-results/
-        done
-        find .out/test-results/ -ls
-    fi
-}
-
-trap cleanup EXIT
-trap cleanup ERR
-
-"$RIO_DIR/parallel-tests.sh" ./rio/unittests.yml parallel-output
+# Wrap call to parallel-tests with timeout as per-command
+# timeouts are not currently implemented and this gives us
+# a chance of having Rio examine any created output
+#
+timeout 160m "$RIO_DIR/parallel-tests.sh" ./rio/unittests.yml "$PARALLELOUTPUT"
 
 # Extract the count of errors and failures from the junit reports.
 # ParallelCI will prefix the test XML names with the jvm scope used,
 # then extract the summary testsuite lines and use bash arithmetic
 # to compute a total of errors and failures.
-ERRFAILS="$(( $(find "${BASE_DIR}/build/test/output" -name '*TEST-*.xml' -print0 | \
+ERRFAILS="$(( $(find "${PARALLELOUTPUT}" -name '*TEST-*.xml' -print0 | \
                 xargs -0 grep '^<testsuite' | \
                 sed -e 's/^.*errors="\([0-9]*\)" failures="\([0-9]*\)".*$/+\1+\2/g') ))"
 if [ $((ERRFAILS > 0)) ]
