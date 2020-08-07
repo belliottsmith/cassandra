5c9b0ca0b0385b7d8db1c854e7c3010c6841cfa4
Jon Meredith <jmeredithco@apple.com>
2020-08-07 15:44:21 -0600

    rdar://66642434 (CIE4 Host replacement bootstrap node crashes on startup after receiving last repair info before loading repairs) (#1822)
    Update distributed ChristmasPatchTest to verify restarting re-populates after dtest stop/start.
    Factor out loading repair history, move loading before bootrap takes place to match 3.0
    
    Fixes error when replacing a host in a 4.0 cluster.
    ```
    java.lang.IllegalStateException: Last Successful repair should be empty. Org.apache.cassandra.db.xmas.SuccessfulRepairTimeHolder@1ac4579f
            at org.apache.cassandra.db.ColumnFamilyStore.setLastSuccessfulRepairs(ColumnFamilyStore.java:1648) ~[cie-cassandra-4.0.0.1.jar:4.0.0.1]
            at org.apache.cassandra.db.ColumnFamilyStore.loadLastSuccessfulRepair(ColumnFamilyStore.java:1627) ~[cie-cassandra-4.0.0.1.jar:4.0.0.1]
            at org.apache.cassandra.service.CassandraDaemon.setup(CassandraDaemon.java:394) [cie-cassandra-4.0.0.1.jar:4.0.0.1]
            at org.apache.cassandra.service.CassandraDaemon.activate(CassandraDaemon.java:659) [cie-cassandra-4.0.0.1.jar:4.0.0.1]
            at org.apache.cassandra.service.CassandraDaemon.main(CassandraDaemon.java:776) [cie-cassandra-4.0.0.1.jar:4.0.0.1]
    ```
    
    Update the in-jvm Dtest to have the Instance load the repair history after enabling ActiveRepair like regular startup.

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -323,6 +323,7 @@ public class CassandraDaemon
             ScheduledExecutors.optionalTasks.scheduleWithFixedDelay(SizeEstimatesRecorder.instance, 30, sizeRecorderInterval, TimeUnit.SECONDS);
 
         ActiveRepairService.instance.start();
+        loadLastSuccessfulRepairTimes();
 
         // Prepared statements
         QueryProcessor.preloadPreparedStatement();
@@ -391,7 +392,6 @@ public class CassandraDaemon
             {
                 for (final ColumnFamilyStore store : cfs.concatWithIndexes())
                 {
-                    store.loadLastSuccessfulRepair(); // CIE Xmas Patch
                     store.reload(); //reload CFs in case there was a change of disk boundaries
                     if (store.getCompactionStrategyManager().shouldBeEnabled())
                     {
@@ -432,6 +432,25 @@ public class CassandraDaemon
             logger.info("Prewarming of auth caches is disabled");
 
         completeSetup();
+    }
+
+    /* CIE Xmas patch: Load last successful repair times for all keyspaces/tables
+     * When bootstrapping the last successful repair time will be requested once
+     * by a listener on the bootstrapper future.
+     */
+    @VisibleForTesting
+    public void loadLastSuccessfulRepairTimes()
+    {
+        for (Keyspace keyspace : Keyspace.all())
+        {
+            for (ColumnFamilyStore cfs : keyspace.getColumnFamilyStores())
+            {
+                for (final ColumnFamilyStore store : cfs.concatWithIndexes())
+                {
+                    store.loadLastSuccessfulRepair();
+                }
+            }
+        }
     }
 
     public void setupVirtualKeyspaces()
