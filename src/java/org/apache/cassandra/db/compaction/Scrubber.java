4e0a050fb8db7616a5a0e1eb979735d001c9e682
Marcus Eriksson <marcus_eriksson@apple.com>
2020-08-17 09:29:26 +0200

    <rdar://problem/61943525> Prepare a tool to repair instances of duplicate primary keys in itmsconnect (#1852)
    scrub: log primary key of collapsed duplicate rows
    
    (rebased-with-history from commit 31d7775f6d521fe59495d52dadb5b7382e5fe69a)
    
    (rebased-with-history from commit bbe6e3e13eed2b3bdc4fca729704ad205c5f43c5)
    
    (rebased-with-history from commit 1e056b5630f32a5de1cb81f08e829ab6c5d73f9b)

--- src/java/org/apache/cassandra/db/compaction/Scrubber.java
+++ src/java/org/apache/cassandra/db/compaction/Scrubber.java
@@ -359,7 +359,7 @@ public class Scrubber implements Closeable
     @SuppressWarnings("resource")
     private UnfilteredRowIterator getIterator(DecoratedKey key)
     {
-        RowMergingSSTableIterator rowMergingIterator = new RowMergingSSTableIterator(SSTableIdentityIterator.create(sstable, dataFile, key));
+        RowMergingSSTableIterator rowMergingIterator = new RowMergingSSTableIterator(SSTableIdentityIterator.create(sstable, dataFile, key), outputHandler);
         return reinsertOverflowedTTLRows ? new FixNegativeLocalDeletionTimeIterator(rowMergingIterator,
                                                                                     outputHandler,
                                                                                     negativeLocalDeletionInfoMetrics) : rowMergingIterator;
@@ -526,10 +526,12 @@ public class Scrubber implements Closeable
     private static class RowMergingSSTableIterator extends WrappingUnfilteredRowIterator
     {
         Unfiltered nextToOffer = null;
+        private final OutputHandler output;
 
-        RowMergingSSTableIterator(UnfilteredRowIterator source)
+        RowMergingSSTableIterator(UnfilteredRowIterator source, OutputHandler output)
         {
             super(source);
+            this.output = output;
         }
 
         @Override
@@ -545,6 +547,7 @@ public class Scrubber implements Closeable
 
             if (next.isRow())
             {
+                boolean logged = false;
                 while (wrapped.hasNext())
                 {
                     Unfiltered peek = wrapped.next();
@@ -556,6 +559,13 @@ public class Scrubber implements Closeable
 
                     // Duplicate row, merge it.
                     next = Rows.merge((Row) next, (Row) peek);
+
+                    if (!logged)
+                    {
+                        String partitionKey = metadata().partitionKeyType.getString(partitionKey().getKey());
+                        output.warn("Duplicate row detected in " + metadata().keyspace + '.' + metadata().name + ": " + partitionKey + " " + next.clustering().toString(metadata()));
+                        logged = true;
+                    }
                 }
             }
 
