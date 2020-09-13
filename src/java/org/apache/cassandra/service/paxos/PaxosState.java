406fa39357686aa240fdb3f307a9e3a8626754c1
Benedict Elliott Smith <benedict@apache.org>
2020-09-13 13:26:19 +0100

    rdar://68799713 (Paxos: legacyPrepare messages larger than necessary)

--- src/java/org/apache/cassandra/service/paxos/PaxosState.java
+++ src/java/org/apache/cassandra/service/paxos/PaxosState.java
@@ -504,8 +504,10 @@ public class PaxosState implements AutoCloseable
                         if (currentUpdater.compareAndSet(unsafeState, before, after))
                         {
                             Tracing.trace("Promising ballot {}", toPrepare.ballot);
-                            SystemKeyspace.savePaxosPromise(toPrepare.update.partitionKey(), toPrepare.update.metadata(), toPrepare.ballot);
-                            return new PrepareResponse(true, before.accepted == null ? before.committed : before.accepted, before.committed);
+                            DecoratedKey partitionKey = toPrepare.update.partitionKey();
+                            CFMetaData metadata = toPrepare.update.metadata();
+                            SystemKeyspace.savePaxosPromise(partitionKey, metadata, toPrepare.ballot);
+                            return new PrepareResponse(true, before.accepted == null ? Accepted.none(partitionKey, metadata) : before.accepted, before.committed);
                         }
                     }
                     else
