b8f2038903a84548179daa7ba54da6bfdd45e72c
Yifan Cai <yifan_cai@apple.com>
2020-09-21 18:40:21 -0700

    fixup rdar://60088325 p33279387 OPS/COR Don't allow requests for out of token range operations for readRepairRTRangeMovementTest
    allow out of range read in readRepairRTRangeMovementTest

--- test/distributed/org/apache/cassandra/distributed/test/ReadRepairTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/ReadRepairTest.java
@@ -195,7 +195,9 @@ public class ReadRepairTest extends TestBaseImpl
         String key = "test1";
         try (Cluster cluster = init(Cluster.build()
                                            .withConfig(config -> config.with(Feature.GOSSIP, Feature.NETWORK)
-                                                                       .set("read_request_timeout_in_ms", Integer.MAX_VALUE))
+                                                                       .set("read_request_timeout_in_ms", Integer.MAX_VALUE)
+                                                                       .set("reject_out_of_token_range_requests", false)
+                                           )
                                            .withTokenSupplier(TokenSupplier.evenlyDistributedTokens(4))
                                            .withNodeIdTopology(NetworkTopology.singleDcNetworkTopology(4, "dc0", "rack0"))
                                            .withNodes(3)
