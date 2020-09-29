09ddfb06782c76545421bddad6083b4dd8c41a07
Blake Eggleston <beggleston@apple.com>
2020-09-29 16:07:56 -0700

    rdar://69762139 (ValueAccessor is using signed shorts in sliceWithSho… (#1937)

--- src/java/org/apache/cassandra/db/marshal/ByteArrayAccessor.java
+++ src/java/org/apache/cassandra/db/marshal/ByteArrayAccessor.java
@@ -164,6 +164,12 @@ public class ByteArrayAccessor implements ValueAccessor<byte[]>
         return ByteArrayUtil.getShort(value, offset);
     }
 
+    @Override
+    public int getUnsignedShort(byte[] value, int offset)
+    {
+        return ByteArrayUtil.getUnsignedShort(value, offset);
+    }
+
     public int toInt(byte[] value)
     {
         return getInt(value, 0);
