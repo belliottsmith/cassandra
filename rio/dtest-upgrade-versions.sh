21eb8f60b68f3110559294683950ccc1ca7e4093
Jon Meredith <jmeredithco@apple.com>
2020-10-28 10:26:47 -0600

    fixup: rdar://60087087 p21020076 ENV/BLD cie-cassandra on Rio - move dtest upgrade 2.2 tag - 0.0.6 tag

--- rio/dtest-upgrade-versions.sh
+++ rio/dtest-upgrade-versions.sh
@@ -1,4 +1,4 @@
 dtest_upgrade_versions=(
-  "git@github.pie.apple.com:aci/apache-cassandra.git cassandra-2.2-dtest-0.6"
+  "git@github.pie.apple.com:aci/apache-cassandra.git cassandra-2.2-dtest-0.0.6"
   "git@github.geo.apple.com:cie/cie-cassandra.git cie-cassandra-3.0.19.0-hotfix"
 )
