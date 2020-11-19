bd1894ac04456a81b63685493488012e94ce2fce
Benedict Elliott Smith <benedict@apple.com>
2020-11-19 18:00:52 +0000

    rdar://71546131 (failing PaxosRepairHistoryTest (testRandomAdds(-5986708161426773475L, 3, 100, 3, 0.010000f, 0.100000f); )) (#2060)

--- test/unit/org/apache/cassandra/service/paxos/PaxosRepairHistoryTest.java
+++ test/unit/org/apache/cassandra/service/paxos/PaxosRepairHistoryTest.java
@@ -490,10 +490,15 @@ public class PaxosRepairHistoryTest
                 (r.right.isMinimum()
                         ? canonical.subMap(r.left, true, new LongToken(Long.MAX_VALUE), true)
                         : canonical.subMap(r.left, true, r.right, false)
-                ).entrySet().forEach(e -> e.setValue(latest(ballot, e.getValue())));
+                ).entrySet().forEach(e -> e.setValue(latest(e.getValue(), ballot)));
             }
 
         }
     }
 
+    @Test
+    public void testRegression2()
+    {
+        testRandomAdds(-5986708161426773475L, 3, 100, 3, 0.010000f, 0.100000f);
+    }
 }
