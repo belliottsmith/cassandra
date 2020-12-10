daaf3da2e177ce02ad7209d5562730280e11eb18
Jon Meredith <jmeredithco@apple.com>
2020-12-10 16:11:43 -0700

    rdar://72051783 (Migrate cie-cassandra from github.geo to github.pie) (#2090)

--- rio/dtest-upgrade-versions.sh
+++ rio/dtest-upgrade-versions.sh
@@ -1,4 +1,4 @@
 dtest_upgrade_versions=(
   "https://github.pie.apple.com/aci/apache-cassandra.git cassandra-2.2-dtest-0.3"
-  "git@github.geo.apple.com:cie/cie-cassandra.git cie-cassandra-4.0.0"
+  "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-4.0.0"
 )
