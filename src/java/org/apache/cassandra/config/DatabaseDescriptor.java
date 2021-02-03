5570cd2dee8de724670c3a8177752ba03cd87a0c
Chris Lohfink <clohfink@apple.com>
2021-02-03 08:39:03 -0600

    <rdar://74358271> Faster hash path for clusters under high auth churn & add hash translation layer for decreased rounds from old default to new
    This is a forward port to 4.0.0 for rdar://73822264.

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1430,6 +1430,16 @@ public class DatabaseDescriptor
         conf.credentials_cache_active_update = activeUpdate;
     }
 
+    public static boolean getAuthHashCacheEnabled()
+    {
+        return conf.auth_hash_cache;
+    }
+
+    public static void setAuthHashCacheEnabled(boolean authHashCacheEnabled)
+    {
+        conf.auth_hash_cache = authHashCacheEnabled;
+    }
+
     public static int getMaxValueSize()
     {
         return conf.max_value_size_in_mb * 1024 * 1024;
