3f13f4570195d3f393d058d19b549fe3d6082f17
Jon Meredith <jmeredithco@apple.com>
2021-02-23 09:52:37 -0700

    fixup rdar://60087087 p21020076 ENV/BLD cie-cassandra on Rio - revert to cassandra-2.2 fro dtest-api 0.0.7

--- rio/dtest-upgrade-versions.sh
+++ rio/dtest-upgrade-versions.sh
@@ -1,4 +1,4 @@
 dtest_upgrade_versions=(
-  "https://github.pie.apple.com/aci/apache-cassandra.git cassandra-2.2-dtest-0.0.6-CASSANDRA-16256"
+  "https://github.pie.apple.com/aci/apache-cassandra.git cassandra-2.2"
   "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-3.0.19.0-hotfix"
 )
