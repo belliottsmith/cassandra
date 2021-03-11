adbf4a2d5fcfc6f65c90a77e68a908959e92a25e
Alex Petrov <oleksandr.petrov@gmail.com>
2021-03-11 19:04:55 +0100

    rdar://74516874 (Backport CASSANDRA-15679: cqlsh COPY FROM of map of blobs fails with parse error "unhashable type: 'bytearray'")
        Fix parse error in cqlsh COPY FROM and formatting for map of blobs
    
        patch by Aleksandr Sorokoumov; reviewed by Stefania Alborghetti for CASSANDRA-15679

--- pylib/cqlshlib/formatting.py
+++ pylib/cqlshlib/formatting.py
@@ -148,14 +148,21 @@ def formatter_for(typname):
     return registrator
 
 
-@formatter_for('bytearray')
+class BlobType(object):
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        return str(self.val)
+
+@formatter_for('BlobType')
 def format_value_blob(val, colormap, **_):
     bval = '0x' + binascii.hexlify(val)
     return colorme(bval, colormap, 'blob')
 
-
+formatter_for('bytearray')(format_value_blob)
 formatter_for('buffer')(format_value_blob)
-
+formatter_for('blob')(format_value_blob)
 
 def format_python_formatted_type(val, colormap, color, quote=False):
     bval = str(val)
