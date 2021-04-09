c3f5cd4548a4383d9c1112d495960aece565f160
David Capwell <dcapwell@apple.com>
2021-04-09 12:14:29 -0700

    rdar://76421751 (set incremental_updates_last_repaired=false) (#2335)

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -486,7 +486,7 @@ public class Config
     public volatile boolean skip_single_sstable_scheduled_compactions = true;
     public volatile long max_scheduled_compaction_sstable_size_bytes = 10240 * 1024L * 1024L;
     public volatile int max_scheduled_compaction_sstable_count = 40;
-    public volatile boolean incremental_updates_last_repaired = true;
+    public volatile boolean incremental_updates_last_repaired = false;
 
     public volatile long large_partition_index_warning_threshold_kb = 4096L;
     public volatile long large_partition_index_failure_threshold_kb = 0;
