302995c7964ea284d8bb39a1af3bb488e9e0eda9
Saranya Krishnakumar <saranya_k@apple.com>
2021-04-12 09:59:07 -0700

    rdar://75962948 Add BytesReadPerQuery & BytesWrittenPerQuery metrics to StorageProxy (#2319)
    (rebased-with-history from commit c88b19dbefaa37086bbb450eede08b4eaa60aae0)
    
    (rebased-with-history from commit 35f91e95aeaa8669da8f465b0c13bf4e8d7d531d)

---
+++ test/unit/org/apache/cassandra/service/StorageProxyTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cassandra.service;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.UniformReservoir;
+import org.apache.cassandra.metrics.CassandraMetricsRegistry;
+
+import static org.junit.Assert.assertEquals;
+
+public class StorageProxyTest
+{
+    @Test
+    public void testGetRecentValues()
+    {
+        final Histogram histogram = new Histogram(new UniformReservoir());
+        histogram.update(5L);
+
+        final CassandraMetricsRegistry.JmxHistogram jmxHistogram = new CassandraMetricsRegistry.JmxHistogram(histogram, null);
+        assertEquals(1, Arrays.stream(jmxHistogram.getRecentValues()).filter(val -> val != 0).count());
+
+        histogram.update(10L);
+        histogram.update(20L);
+        assertEquals(2, Arrays.stream(jmxHistogram.getRecentValues()).filter(val -> val != 0).count());
+    }
+}
