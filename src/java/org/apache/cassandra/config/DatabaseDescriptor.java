e558304b2c87439983ab979cf4218cf13e28ea99
Chris Lohfink <clohfink@apple.com>
2021-08-09 13:16:56 -0500

    rdar://71703399 (i2 building should not block compactions from being executed)

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1886,6 +1886,11 @@ public class DatabaseDescriptor
         conf.compaction_tombstone_warning_threshold = count;
     }
 
+    public static int getConcurrentIndexBuilds()
+    {
+        return conf.concurrent_index_builds;
+    }
+
     public static int getConcurrentValidations()
     {
         return conf.concurrent_validations;
