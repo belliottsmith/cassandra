c78e33410c6bcf4cc258414eee68de057d2486f1
Sam Tunnicliffe <samt@apple.com>
2021-08-20 14:19:51 +0100

    Enable Xmas patch to be disabled on a per-table level via CQL and hotprop (#2495)
    Overriding the instance-level config, from yaml, is off by default, but operators can disable christmas
    patch for a specific table using '(WITH|AND) disable_christmas_patch=true' in a CREATE/ALTER
    statement. This only has effect if enable_christmas_patch: true is set in cassandra.yaml.
    
    rdar://59244202 (Add a ColumnFamily level ChristmasPatch setting) (#2126)
    
    Co-authored-by: Chris <cearman@apple.com>
    Co-authored-by: Marcus Eriksson <marcus_eriksson@apple.com>
    
    Forward ported to 4.0.0 for rdar://78757030 (Forward port rdar://59244202 (Add a ColumnFamily level
    ChristmasPatch setting) to 4.0)
    
    (rebased-with-history from commit 370ab6b1f8e9fe1d98d9b1a3f4221220fb748ae6)
    
    (rebased-with-history from commit f74035b695d3274436475c6bc0fb01130af4b13f)

--- src/java/org/apache/cassandra/cql3/statements/schema/TableAttributes.java
+++ src/java/org/apache/cassandra/cql3/statements/schema/TableAttributes.java
@@ -151,6 +151,9 @@ public final class TableAttributes extends PropertyDefinitions
         if (hasOption(Option.READ_REPAIR))
             builder.readRepair(ReadRepairStrategy.fromString(getString(Option.READ_REPAIR)));
 
+        if (hasOption(Option.DISABLE_CHRISTMAS_PATCH))
+            builder.disableChristmasPatch(getBoolean(Option.DISABLE_CHRISTMAS_PATCH));
+
         return builder.build();
     }
 
@@ -209,6 +212,12 @@ public final class TableAttributes extends PropertyDefinitions
         if (value == null)
             throw new IllegalStateException(format("Option '%s' is absent", option));
         return value;
+    }
+
+    private Boolean getBoolean(Option option)
+    {
+        String value = getSimple(option.toString());
+        return Boolean.valueOf(value);
     }
 
     public boolean hasOption(Option option)
