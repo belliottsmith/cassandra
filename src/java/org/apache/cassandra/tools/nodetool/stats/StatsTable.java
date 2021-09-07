c96c6ec1b91fc319a30206ea9073f85c9abd7ba5
Stefan Podkowinski <spodkowinski@apple.com>
2021-09-07 10:31:58 +0200

    rdar://82811671 (4.0: port implement SSTable count by TWCS bucket) (#2553)

--- src/java/org/apache/cassandra/tools/nodetool/stats/StatsTable.java
+++ src/java/org/apache/cassandra/tools/nodetool/stats/StatsTable.java
@@ -71,6 +71,7 @@ public class StatsTable
     public String droppedMutations;
     public List<String> sstablesInEachLevel = new ArrayList<>();
     public List<String> sstableBytesInEachLevel = new ArrayList<>();
+    public int[] sstableCountPerTWCSBucket = null;
     public Boolean isInCorrectLocation = null; // null: option not active
     public Map<String, String> topSizePartitions;
     public Map<String, Long> topTombstonePartitions;
