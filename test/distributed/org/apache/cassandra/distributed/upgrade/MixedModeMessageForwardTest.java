8e7a39e7e77118725ef6e0f44883e56613a8094d
Jon Meredith <jmeredithco@apple.com>
2021-10-07 10:59:36 -0600

    rdar://83972834 (Test failure, MixedModeMessageForwardTest.checkWritesForwardedToOtherDcTest)
    Wrong timeout was set.  It needs to be the `write_request_timeout_in_ms`. Previous test failure
    was due to connection setup taking more than 2s for the first upgraded node.

--- test/distributed/org/apache/cassandra/distributed/upgrade/MixedModeMessageForwardTest.java
+++ test/distributed/org/apache/cassandra/distributed/upgrade/MixedModeMessageForwardTest.java
@@ -81,7 +81,7 @@ public class MixedModeMessageForwardTest extends UpgradeTestBase
                                   .collect(Collectors.joining(","));
 
         new TestCase()
-        .withConfig(c -> c.with(Feature.GOSSIP, Feature.NETWORK).set("request_timeout_in_ms", 30000))
+        .withConfig(c -> c.with(Feature.GOSSIP, Feature.NETWORK).set("write_request_timeout_in_ms", 30000))
         .withBuilder(b -> b.withRacks(numDCs, 1, nodesPerDc))
         .nodes(numDCs * nodesPerDc)
         .singleUpgrade(v30, v40)
