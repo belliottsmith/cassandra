d8d33558f53e4899cd8e3d19e337c07de4140699
Yifan Cai <yifan_cai@apple.com>
2021-11-02 15:33:33 -0700

    Forward-port rdar://77771062 Ability to log load profiles at fixed intervals (#2568)
    (rebased-with-history from commit 422e3f411b48c140f991175c11bd127eb41e58d5)
    
    (rebased-with-history from commit 87b03352d52e5c14f4bf96777a7e1292ac203038)

--- test/unit/org/apache/cassandra/tools/TopPartitionsTest.java
+++ test/unit/org/apache/cassandra/tools/TopPartitionsTest.java
@@ -18,29 +18,36 @@
 
 package org.apache.cassandra.tools;
 
-import static java.lang.String.format;
-import static org.apache.cassandra.cql3.QueryProcessor.executeInternal;
-import static org.junit.Assert.assertEquals;
-
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
+import java.util.stream.Collectors;
 import javax.management.openmbean.CompositeData;
+
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import org.apache.cassandra.SchemaLoader;
 import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.SystemKeyspace;
 import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.metrics.Sampler;
 import org.apache.cassandra.service.StorageService;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
-import com.google.common.collect.Lists;
+import static java.lang.String.format;
+import static org.apache.cassandra.cql3.QueryProcessor.executeInternal;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+// Includes test cases for 'toppartitions' and the successor 'profileload'
 public class TopPartitionsTest
 {
     @BeforeClass
@@ -58,7 +65,7 @@ public class TopPartitionsTest
         {
             try
             {
-                q.put(StorageService.instance.samplePartitions(1000, 100, 10, Lists.newArrayList("READS", "WRITES")));
+                q.put(StorageService.instance.samplePartitions(null, 1000, 100, 10, Lists.newArrayList("READS", "WRITES")));
             }
             catch (Exception e)
             {
@@ -80,5 +87,25 @@ public class TopPartitionsTest
         executeInternal(format(req, SystemKeyspace.LOCAL, SystemKeyspace.LOCAL));
         List<CompositeData> result = ColumnFamilyStore.getIfExists("system", "local").finishLocalSampling("READS", 5);
         assertEquals(1, result.size());
+    }
+
+    @Test
+    public void testStartAndStopScheduledSampling()
+    {
+        List<String> allSamplers = Arrays.stream(Sampler.SamplerType.values()).map(Enum::toString).collect(Collectors.toList());
+        StorageService ss = StorageService.instance;
+        assertTrue("It should allow a new scheduled sampling task",
+                   ss.startSamplingPartitions(null, null, 10, 10, 100, 10, allSamplers));
+        assertEquals(Collections.singletonList("*.*"), ss.getSampleTasks());
+        assertFalse("It should not allow start sampling with the same key",
+                    ss.startSamplingPartitions(null, null, 20, 20, 100, 10, allSamplers));
+        assertTrue("It should allow to stop an existing scheduled sampling task",
+                   ss.stopSamplingPartitions(null, null));
+        int timeout = 3;
+        while (timeout-- > 0 && ss.getSampleTasks().size() > 0)
+            Uninterruptibles.sleepUninterruptibly(10, TimeUnit.MILLISECONDS);
+        assertEquals("It should remove the scheduled sample task.", Collections.emptyList(), ss.getSampleTasks());
+        assertTrue("It should allow stop all scheduled sampling task when nothing scheduled",
+                    ss.stopSamplingPartitions(null, null));
     }
 }
