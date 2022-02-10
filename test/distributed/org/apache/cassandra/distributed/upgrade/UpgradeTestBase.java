1773177be4c1c8092c81e0a40d8c3ac857274c38
Jon Meredith <jmeredithco@apple.com>
2022-02-10 16:12:06 -0700

    Update upgrade paths for ACI Cassandra supported upgrades

--- test/distributed/org/apache/cassandra/distributed/upgrade/UpgradeTestBase.java
+++ test/distributed/org/apache/cassandra/distributed/upgrade/UpgradeTestBase.java
@@ -87,11 +87,7 @@ public class UpgradeTestBase extends DistributedTestBase
 
     protected static final List<Pair<Semver,Semver>> SUPPORTED_UPGRADE_PATHS = ImmutableList.of(
         Pair.create(v22, v30),
-        Pair.create(v22, v3X),
-        Pair.create(v30, v3X),
-        Pair.create(v30, v40),
-        Pair.create(v3X, v40),
-        Pair.create(v40, v41));
+        Pair.create(v30, v40));
 
     // the last is always the current
     public static final Semver CURRENT = SUPPORTED_UPGRADE_PATHS.get(SUPPORTED_UPGRADE_PATHS.size() - 1).right;
