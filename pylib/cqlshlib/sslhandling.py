181375211db36f08ce3b484b54463633e9db4fc8
Sam Tunnicliffe <samt@apple.com>
2022-06-07 18:28:27 +0100

    Partially revert c408931aa262353845c926f367ad06d27d9f164e
    fix warnings in pylib tests
    
    patch by Brad Schoening; reviewed by Stefan Miklosovic and Brandon Williams for CASSANDRA-17397

--- pylib/cqlshlib/sslhandling.py
+++ pylib/cqlshlib/sslhandling.py
@@ -18,7 +18,7 @@ import os
 import sys
 import ssl
 
-import configparser
+from six.moves import configparser
 
 
 def ssl_settings(host, config_file, env=os.environ):
@@ -39,7 +39,7 @@ def ssl_settings(host, config_file, env=os.environ):
     either in the config file or as an environment variable.
     Environment variables override any options set in cqlsh config file.
     """
-    configs = configparser.ConfigParser()
+    configs = configparser.SafeConfigParser()
     configs.read(config_file)
 
     def get_option(section, option):
