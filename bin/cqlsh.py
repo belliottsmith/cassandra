4bfcba39be233f862833b2495c2a517069c45518
Sam Tunnicliffe <samt@apple.com>
2022-06-07 18:23:04 +0100

    Revert "add credentials file support for CQLSH"
    This reverts commit 917d74bd35712f8374ecefd1d890d02916162daa.

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -19,13 +19,11 @@
 import cmd
 import codecs
 import csv
-import errno
 import getpass
 import optparse
 import os
 import platform
 import re
-import stat
 import subprocess
 import sys
 import traceback
@@ -205,7 +203,6 @@ parser.add_option('--coverage', action='store_true',
 parser.add_option("--encoding", help="Specify a non-default encoding for output."
                   + " (Default: %s)" % (UTF8,))
 parser.add_option("--cqlshrc", help="Specify an alternative cqlshrc file location.")
-parser.add_option("--credentials", help="Specify an alternative credentials file location.")
 parser.add_option('--cqlversion', default=None,
                   help='Specify a particular CQL version, '
                        'by default the highest version supported by the server will be used.'
@@ -221,13 +218,6 @@ parser.add_option("--request-timeout", default=DEFAULT_REQUEST_TIMEOUT_SECONDS, 
 parser.add_option("-t", "--tty", action='store_true', dest='tty',
                   help='Force tty mode (command prompt).')
 parser.add_option('-v', action="version", help='Print the current version of cqlsh.')
-
-# This is a hidden option to suppress the warning when the -p/--password command line option is used.
-# Power users may use this option if they know no other people has access to the system where cqlsh is run or don't care about security.
-# Use of this option in scripting is discouraged. Please use a (temporary) credentials file where possible.
-# The Cassandra distributed tests (dtests) also use this option in some tests when a well-known password is supplied via the command line.
-parser.add_option("--insecure-password-without-warning", action='store_true', dest='insecure_password_without_warning',
-                  help=optparse.SUPPRESS_HELP)
 
 optvalues = optparse.Values()
 (options, arguments) = parser.parse_args(sys.argv[1:], values=optvalues)
@@ -2136,21 +2126,6 @@ def should_use_color():
     return True
 
 
-def is_file_secure(filename):
-    try:
-        st = os.stat(filename)
-    except OSError as e:
-        if e.errno != errno.ENOENT:
-            raise
-        return True  # the file doesn't exists, the security of it is irrelevant
-
-    uid = os.getuid()
-
-    # Skip enforcing the file owner and UID matching for the root user (uid == 0).
-    # This is to allow "sudo cqlsh" to work with user owned credentials file.
-    return (uid == 0 or st.st_uid == uid) and stat.S_IMODE(st.st_mode) & (stat.S_IRGRP | stat.S_IROTH) == 0
-
-
 def read_options(cmdlineargs, environment):
     configs = configparser.SafeConfigParser() if sys.version_info < (3, 2) else configparser.ConfigParser()
     configs.read(CONFIG_FILE)
@@ -2158,20 +2133,9 @@ def read_options(cmdlineargs, environment):
     rawconfigs = configparser.RawConfigParser()
     rawconfigs.read(CONFIG_FILE)
 
-    username_from_cqlshrc = option_with_default(configs.get, 'authentication', 'username')
-    password_from_cqlshrc = option_with_default(rawconfigs.get, 'authentication', 'password')
-    if username_from_cqlshrc or password_from_cqlshrc:
-        if password_from_cqlshrc and not is_file_secure(CONFIG_FILE):
-            print("\nWarning: Password is found in an insecure cqlshrc file. The file is owned or readable by other users on the system.",
-                  end='', file=sys.stderr)
-        print("\nNotice: Credentials in the cqlshrc file is deprecated and will be ignored in the future."
-              "\nPlease use a credentials file to specify the username and password.\n", file=sys.stderr)
-
     optvalues = optparse.Values()
-    optvalues.username = None
-    optvalues.password = None
-    optvalues.credentials = os.path.expanduser(option_with_default(configs.get, 'authentication', 'credentials',
-                                                                   os.path.join(CQL_DIR, 'credentials')))
+    optvalues.username = option_with_default(configs.get, 'authentication', 'username')
+    optvalues.password = option_with_default(rawconfigs.get, 'authentication', 'password')
     optvalues.keyspace = option_with_default(configs.get, 'authentication', 'keyspace')
     optvalues.browser = option_with_default(configs.get, 'ui', 'browser', None)
     optvalues.completekey = option_with_default(configs.get, 'ui', 'completekey',
@@ -2208,38 +2172,8 @@ def read_options(cmdlineargs, environment):
     optvalues.connect_timeout = option_with_default(configs.getint, 'connection', 'timeout', DEFAULT_CONNECT_TIMEOUT_SECONDS)
     optvalues.request_timeout = option_with_default(configs.getint, 'connection', 'request_timeout', DEFAULT_REQUEST_TIMEOUT_SECONDS)
     optvalues.execute = None
-    optvalues.insecure_password_without_warning = False
 
     (options, arguments) = parser.parse_args(cmdlineargs, values=optvalues)
-
-    if not is_file_secure(options.credentials):
-        print("\nWarning: Credentials file '{0}' exists but is not used, because:"
-              "\n  a. the file owner is not the current user; or"
-              "\n  b. the file is readable by group or other."
-              "\nPlease ensure the file is owned by the current user and is not readable by group or other."
-              "\nOn a Linux or UNIX-like system, you often can do this by using the `chown` and `chmod` commands:"
-              "\n  chown YOUR_USERNAME credentials"
-              "\n  chmod 600 credentials\n".format(options.credentials),
-              file=sys.stderr)
-        options.credentials = ''  # ConfigParser.read() will ignore unreadable files
-
-    if not options.username:
-        credentials = configparser.SafeConfigParser() if sys.version_info < (3, 2) else configparser.ConfigParser()
-        credentials.read(options.credentials)
-
-        # use the username from credentials file but fallback to cqlshrc if username is absent from the command line parameters
-        options.username = option_with_default(credentials.get, 'plain_text_auth', 'username', username_from_cqlshrc)
-
-    if not options.password:
-        rawcredentials = configparser.RawConfigParser()
-        rawcredentials.read(options.credentials)
-
-        # handling password in the same way as username, priority cli > credentials > cqlshrc
-        options.password = option_with_default(rawcredentials.get, 'plain_text_auth', 'password', password_from_cqlshrc)
-    elif not options.insecure_password_without_warning:
-        print("\nWarning: Using a password on the command line interface can be insecure."
-              "\nRecommendation: use the credentials file to securely provide the password.\n", file=sys.stderr)
-
     # Make sure some user values read from the command line are in unicode
     options.execute = maybe_ensure_text(options.execute)
     options.username = maybe_ensure_text(options.username)
