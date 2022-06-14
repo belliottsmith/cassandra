0f2e44ca0dedcbafef3129c00539eb54fe2deca7
Jon Meredith <jmeredithco@apple.com>
2022-06-14 16:45:03 -0600

    fixup: set 4.0 dtest upgrade version to 4.0.2 to pick up newer dtest-api

--- rio/dtest-upgrade-versions.sh
+++ rio/dtest-upgrade-versions.sh
@@ -1,4 +1,4 @@
 dtest_upgrade_versions=(
   "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-3.0.24"
-  "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-4.0.0"
+  "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-4.0.2"
 )
