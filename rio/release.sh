8997070faa715a48e8f3ad0519a0573012c74455
Jon Meredith <jmeredithco@apple.com>
2022-06-23 11:47:59 -0600

    fixup! rdar://60087087 p21020076 ENV/BLD cie-cassandra on Rio

--- rio/release.sh
+++ rio/release.sh
@@ -19,7 +19,8 @@ VERSION="$MAJOR_VERSION"
 
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 ant -f rio-build.xml -Drelease=true -Dbase.version="${VERSION}" realclean
-ant -f rio-build.xml -Drelease=true -Dbase.version="${VERSION}" artifacts dtest-jar sources-jar javadoc-jar
+ant -f rio-build.xml -Drelease=true -Dbase.version="${VERSION}" artifacts dtest-jar sources-jar
+ant -f rio-build.xml -Drelease=true -Dbase.version="${VERSION}" javadoc-jar || true
 
 GROUP_DIR=.dist/publishable/com/apple/cie/db/cassandra
 
