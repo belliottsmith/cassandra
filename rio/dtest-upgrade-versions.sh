c210663714dce62558e030a2601155d67f1a929e
Blake Eggleston <blake@ultrablake.com>
2022-07-13 13:18:41 -0700

    add 61-hotfix dtest jar to upgrade version jars

--- rio/dtest-upgrade-versions.sh
+++ rio/dtest-upgrade-versions.sh
@@ -1,4 +1,5 @@
 dtest_upgrade_versions=(
   "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-3.0.24"
   "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-4.0.2"
+  "git@github.pie.apple.com:aci-cassandra/aci-cassandra.git cie-cassandra-3.0.19.61-hotfix"
 )
