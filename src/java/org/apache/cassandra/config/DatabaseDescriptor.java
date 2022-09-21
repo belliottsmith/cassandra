f368041d732e99e3235c6436d6a397342dba51e2
David Capwell <dcapwell@apple.com>
2022-09-21 10:13:46 -0700

    rdar://95209681 (Host replacement failing waiting on schema of host to be replaced) (#3111)

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -4892,4 +4892,9 @@ public class DatabaseDescriptor
     {
         return conf.host_replacement_allow_status;
     }
+
+    public static boolean getHostReplacementFilterSameRackEnabled()
+    {
+        return conf.host_replacement_filter_same_rack_enabled;
+    }
 }
