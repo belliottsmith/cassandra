950eeed9381506c7b87e405fec8af63445e24bb1
Jon Meredith <jmeredithco@apple.com>
2022-11-17 12:07:28 -0700

    fixup! rdar://92665709 (Improve the way we handle repair message timeouts/fa… (#2920)

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -26,7 +26,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 import javax.annotation.Nullable;
