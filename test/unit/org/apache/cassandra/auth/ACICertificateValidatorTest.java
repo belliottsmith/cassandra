19cc6d281d21a3ea52d907b0ea5a339b50514c14
jkonisa <jkonisa@apple.com>
2023-02-21 16:14:27 -0800

    rdar://102161217 Adding AciCertificateValidator for mTLS authenticators & ACI specific tests

---
+++ test/unit/org/apache/cassandra/auth/ACICertificateValidatorTest.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.auth;
+
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.Collection;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.apache.cassandra.auth.AuthTestUtils.loadCertificateChain;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class ACICertificateValidatorTest
+{
+    @Parameterized.Parameter(0)
+    public String certificatePath;
+    @Parameterized.Parameter(1)
+    public String identity;
+
+    @Parameterized.Parameters()
+    public static Collection<Object[]> versions()
+    {
+        return AuthTestUtils.setupParameterizedTestWithVariousCertificateTypes();
+    }
+
+    @BeforeClass
+    public static void setup()
+    {
+        System.setProperty("cassandra.issueingcertificate.dsid", "1405206");
+    }
+
+    @Test
+    public void isValidCertTest() throws CertificateException
+    {
+        final ACICertificateValidator validator = new ACICertificateValidator();
+        final Certificate[] chain = loadCertificateChain(certificatePath);
+        assertTrue(validator.isValidCertificate(chain));
+    }
+
+    @Test
+    public void isValidCertWithInvalidCertTest() throws CertificateException
+    {
+        final ACICertificateValidator validator = new ACICertificateValidator();
+        final Certificate[] chain = loadCertificateChain("auth/SampleInvalidCertificate.pem");
+        assertFalse(validator.isValidCertificate(chain));
+    }
+
+    @Test
+    public void getIdentityTest() throws CertificateException
+    {
+        final ACICertificateValidator validator = new ACICertificateValidator();
+        final Certificate[] chain = loadCertificateChain(certificatePath);
+        assertEquals(identity, validator.identity(chain));
+    }
+
+    @Test
+    public void validateIdentityTest()
+    {
+        // URN should be a valid identity
+        assertTrue(ACICertificateValidator.validateIdentity("urn:certmanager:idmsGroup/845340"));
+        // a non urn should return false
+        assertFalse(ACICertificateValidator.validateIdentity("non-urn"));
+    }
+}
---
+++ test/unit/org/apache/cassandra/auth/ACIMutualTlsAuthenticatorTest.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.auth;
+
+import java.io.IOException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.Collection;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import org.apache.cassandra.exceptions.AuthenticationException;
+
+import static org.apache.cassandra.auth.AuthTestUtils.getMockInetAddress;
+import static org.apache.cassandra.auth.AuthTestUtils.initializeIdentityRolesTable;
+import static org.apache.cassandra.auth.AuthTestUtils.loadCertificateChain;
+import static org.junit.Assert.assertEquals;
+
+public class ACIMutualTlsAuthenticatorTest extends MutualTlsAuthenticatorTest
+{
+    @Parameterized.Parameters()
+    public static Collection<Object[]> versions()
+    {
+        return AuthTestUtils.setupParameterizedTestWithVariousCertificateTypes();
+    }
+
+    @BeforeClass
+    public static void setup()
+    {
+        System.setProperty("cassandra.issueingcertificate.dsid", "1405206");
+        MutualTlsAuthenticatorTest.setup();
+    }
+
+    @After
+    public void after() throws IOException, TimeoutException
+    {
+        super.after();
+        System.setProperty("cassandra.issueingcertificate.dsid", "1405206");
+    }
+
+    String getValidatorClass()
+    {
+        return "org.apache.cassandra.auth.ACICertificateValidator";
+    }
+
+    @Test
+    public void testGDBCCertONGDBCCluster() throws IOException, TimeoutException, CertificateException
+    {
+        // set cassandra.issueingcertificate.dsid property to GDBC dsid
+        System.setProperty("cassandra.issueingcertificate.dsid", "1405206");
+        initializeIdentityRolesTable("urn:appcertname:identityName/sredbv42-sredb-cassandra.cassandra-prod.kube");
+
+        final Certificate[] chain = loadCertificateChain("auth/SampleGDBCCertificate.pem");
+        final MutualTlsAuthenticator authenticator = createAndInitializeMtlsAuthenticator();
+        final IAuthenticator.SaslNegotiator saslNegotiator = authenticator.newSaslNegotiator(getMockInetAddress(), chain);
+        assertEquals("readonly_user", saslNegotiator.getAuthenticatedUser().getName());
+    }
+
+    @Test
+    public void testGDBCCertONNonGDBCCluster() throws IOException, TimeoutException, CertificateException
+    {
+        // set cassandra.issueingcertificate.dsid property to Non-GDBC dsid
+        System.setProperty("cassandra.issueingcertificate.dsid", "1399644");
+        initializeIdentityRolesTable("urn:appcertname:identityName/sredbv42-sredb-cassandra.cassandra-prod.kube");
+
+        final Certificate[] chain = loadCertificateChain("auth/SampleGDBCCertificate.pem");
+        final MutualTlsAuthenticator authenticator = createAndInitializeMtlsAuthenticator();
+        final IAuthenticator.SaslNegotiator saslNegotiator = authenticator.newSaslNegotiator(getMockInetAddress(), chain);
+        expectedException.expect(AuthenticationException.class);
+        expectedException.expectMessage("Invalid or not supported certificate");
+        saslNegotiator.getAuthenticatedUser();
+    }
+
+    @Test
+    public void testInvalidUsers() throws CertificateException, IOException, TimeoutException
+    {
+        initializeIdentityRolesTable(identity);
+        final Certificate[] clientCertificates = loadCertificateChain("auth/SampleInvalidCertificate.pem");
+        final IAuthenticator mutualTlsAuthenticator = createAndInitializeMtlsAuthenticator();
+        final IAuthenticator.SaslNegotiator saslNegotiator = mutualTlsAuthenticator.newSaslNegotiator(getMockInetAddress(), clientCertificates);
+        expectedException.expect(AuthenticationException.class);
+        expectedException.expectMessage("Invalid or not supported certificate");
+        saslNegotiator.getAuthenticatedUser();
+    }
+}
