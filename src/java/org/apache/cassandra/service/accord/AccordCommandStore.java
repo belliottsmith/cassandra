0453c3dc22a182a243af255eeb38edaae4d6ccf7
Alex Petrov <oleksandr.petrov@gmail.com>
2024-07-22 12:44:41 +0200

    Make application async

--- src/java/org/apache/cassandra/service/accord/AccordCommandStore.java
+++ src/java/org/apache/cassandra/service/accord/AccordCommandStore.java
@@ -341,7 +341,7 @@ public class AccordCommandStore extends CommandStore implements CacheSize
 
         // TODO (required): make sure we test recovering when this has failed to be persisted
         if (null != mutation)
-            mutation.applyUnsafe();
+            return mutation::applyUnsafe;
 
         return NO_OP;
     }
